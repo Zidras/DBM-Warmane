1f1a15bf8fd63acf1791c417e0f3f3a45c991a2f
Andrew <andrewseward93@gmail.com>
2020-07-13 19:31:01 -0700

    bump version

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -58,10 +58,10 @@ f:SetScript("OnUpdate", fCLFix)
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 5010 $"):sub(12, -3),
-	Version = "5.01",
-	DisplayVersion = "5.01 DBM-Frostmourne. Original edit by Sariyo.", -- the string that is shown as version
-	ReleaseRevision = 5010 -- the revision of the latest stable version that is available (for /dbm ver2)
+	Revision = ("$Revision: 5020 $"):sub(12, -3),
+	Version = "5.02",
+	DisplayVersion = "5.02 DBM-Frostmourne. Original edit by Sariyo.", -- the string that is shown as version
+	ReleaseRevision = 5020 -- the revision of the latest stable version that is available (for /dbm ver2)
 }
 
 DBM_SavedOptions = {}
