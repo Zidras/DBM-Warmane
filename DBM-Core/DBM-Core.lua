4530d650b993a38cb88efebaa428675ad659b07e
Filipp Balakin <filipp@balakin.ru>
2020-08-18 18:24:24 +0300

    bump version

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -58,10 +58,10 @@ f:SetScript("OnUpdate", fCLFix)
 --  Globals/Default Options  --
 -------------------------------
 DBM = {
-	Revision = ("$Revision: 6003 $"):sub(12, -3),
-	Version = "6.03",
-	DisplayVersion = "6.03 DBM-WoWCircle by Barsoom for WoWCircle WotLK (https://github.com/ArsumPB/DBM-wowcircle)", -- the string that is shown as version
-	ReleaseRevision = 6003 -- the revision of the latest stable version that is available (for /dbm ver2)
+	Revision = ("$Revision: 6004 $"):sub(12, -3),
+	Version = "6.04",
+	DisplayVersion = "6.04 DBM-WoWCircle by Barsoom for WoWCircle WotLK (https://github.com/ArsumPB/DBM-wowcircle)", -- the string that is shown as version
+	ReleaseRevision = 6004 -- the revision of the latest stable version that is available (for /dbm ver2)
 }
 
 DBM_SavedOptions = {}
