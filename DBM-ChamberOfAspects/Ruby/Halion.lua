f441af13857f1c8baf2fa12aeb9eac754274a544
Zidras <10605951+Zidras@users.noreply.github.com>
2021-06-11 00:37:38 +0100

    Halion: attempt fix cutter double announce

--- DBM-ChamberOfAspects/Ruby/Halion.lua
+++ DBM-ChamberOfAspects/Ruby/Halion.lua
@@ -190,13 +190,13 @@ function mod:CHAT_MSG_MONSTER_YELL(msg)
 			timerTwilightCutterCD:Schedule(15)
 		end
 		if self:LatencyCheck() then
-			self:SendSync("TwilightCutter")
+			self:SendSync("TwilightCutterNew")
 		end
 	end
 end
 
 function mod:OnSync(msg, target)
-	if msg == "TwilightCutter" then
+	if msg == "TwilightCutterNew" then
 		if self.Options.AnnounceAlternatePhase then
 			timerTwilightCutterCD:Cancel()
 			warningTwilightCutter:Show()
