565e666c467808c76ad9d3d637f1151ee9b29009
Filipp Balakin <filipp@balakin.ru>
2021-06-24 23:10:13 +0300

    SetPcoldIcons

--- DBM-Coliseum/Anub'arak.lua
+++ DBM-Coliseum/Anub'arak.lua
@@ -164,9 +164,8 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 		if self.Options.SetIconsOnPCold then
 			table.insert(PColdTargets, DBM:GetRaidUnitId(args.destName))
-			if ((mod:IsDifficulty("normal25") or mod:IsDifficulty("heroic25")) and #PColdTargets >= 5) or ((mod:IsDifficulty("normal10") or mod:IsDifficulty("heroic10")) and #PColdTargets >= 2) then
-				self:SetPcoldIcons()--Sort and fire as early as possible once we have all targets.
-			end
+			self:UnscheduleMethod("SetPcoldIcons")
+			self:ScheduleMethod(0.5, "SetPcoldIcons")
 		end
 		timerPCold:Show()
 	elseif args:IsSpellID(66012) then							-- Freezing Slash
