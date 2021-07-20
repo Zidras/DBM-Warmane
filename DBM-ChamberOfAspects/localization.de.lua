8df8fd87eaf9a08434270aa6a46009b7d4b73fa1
Ayaro <Ayaro1@users.noreply.github.com>
2021-07-20 21:34:36 +0200

    Create localization.de.lua

---
+++ DBM-ChamberOfAspects/localization.de.lua
@@ -0,0 +1,176 @@
+if GetLocale() ~= "deDE" then return end
+
+local L
+
+----------------------------
+--  The Obsidian Sanctum  --
+----------------------------
+--  Shadron  --
+---------------
+L = DBM:GetModLocalization("Shadron")
+
+L:SetGeneralLocalization({
+	name = "Shadron"
+})
+
+----------------
+--  Tenebron  --
+----------------
+L = DBM:GetModLocalization("Tenebron")
+
+L:SetGeneralLocalization({
+	name = "Tenebron"
+})
+
+----------------
+--  Vesperon  --
+----------------
+L = DBM:GetModLocalization("Vesperon")
+
+L:SetGeneralLocalization({
+	name = "Vesperon"
+})
+
+------------------
+--  Sartharion  --
+------------------
+L = DBM:GetModLocalization("Sartharion")
+
+L:SetGeneralLocalization({
+	name = "Sartharion"
+})
+
+L:SetWarningLocalization({
+	WarningTenebron			= "Tenebron kommt",
+	WarningShadron			= "Shadron kommt",
+	WarningVesperon			= "Vesperon kommt",
+	WarningFireWall			= "Feuerwand",
+	WarningVesperonPortal	= "Vesperons Portal",
+	WarningTenebronPortal	= "Tenebrons Portal",
+	WarningShadronPortal	= "Shadrons Portal"
+})
+
+L:SetTimerLocalization({
+	TimerTenebron	= "Tenebron kommt",
+	TimerShadron	= "Shadron kommt",
+	TimerVesperon	= "Vesperon kommt"
+})
+
+L:SetOptionLocalization({
+	PlaySoundOnFireWall		= "Spiele Sound bei Feuerwand",
+	AnnounceFails			= "Verkünde Spieler, die bei Feuerwand und Zone der Leere scheitern, im Raidchat (benötigt aktivierte Ankündigungen und (L)- oder (A)-Status)",
+	TimerTenebron			= "Zeige Timer für Tenebrons Ankunft",
+	TimerShadron			= "Zeige Timer für Shadrons Ankunft",
+	TimerVesperon			= "Zeige Timer für Vesperons Ankunft",
+	WarningFireWall			= "Zeige Spezialwarnung für Feuerwand",
+	WarningTenebron			= "Verkünde Ankunft von Tenebron",
+	WarningShadron			= "Verkünde Ankunft von Shadron",
+	WarningVesperon			= "Verkünde Ankunft von Vesperon",
+	WarningTenebronPortal	= "Zeige Spezialwarnung für Tenebrons Portale",
+	WarningShadronPortal	= "Zeige Spezialwarnung für Shadrons Portale",
+	WarningVesperonPortal	= "Zeige Spezialwarnung für Vesperons Portale"
+})
+
+L:SetMiscLocalization({
+	Wall			= "Die Lava um %s brodelt!",
+	Portal			= "%s beginnt, ein Portal des Zwielichts zu öffnen!",
+	NameTenebron	= "Tenebron",
+	NameShadron		= "Shadron",
+	NameVesperon	= "Vesperon",
+	FireWallOn		= "Feuerwand: %s",
+	VoidZoneOn		= "Schattenspalt: %s",
+	VoidZones		= "Fehler bei Schattenspalt (dieser Versuch): %s",
+	FireWalls		= "Fehler bei Feuerwand (dieser Versuch): %s"
+})
+
+------------------------
+--  The Ruby Sanctum  --
+------------------------
+--  Baltharus the Warborn  --
+-----------------------------
+L = DBM:GetModLocalization("Baltharus")
+
+L:SetGeneralLocalization({
+	name = "Baltharus der Kriegsjünger"
+})
+
+L:SetWarningLocalization({
+	WarningSplitSoon	= "Aufspaltung bald"
+})
+
+L:SetOptionLocalization({
+	WarningSplitSoon	= "Zeige Vorwarnun für Aufspaltung",
+	RangeFrame			= "Zeige Abstandsfenster (12m)",
+	SetIconOnBrand		= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(74505)
+})
+
+-------------------------
+--  Saviana Ragefire  --
+-------------------------
+L = DBM:GetModLocalization("Saviana")
+
+L:SetGeneralLocalization({
+	name = "Saviana Flammenschlund"
+})
+
+L:SetOptionLocalization({
+	RangeFrame				= "Zeige Abstandsfenster (10 m)",
+	BeaconIcon				= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(74453)
+})
+
+--------------------------
+--  General Zarithrian  --
+--------------------------
+L = DBM:GetModLocalization("Zarithrian")
+
+L:SetGeneralLocalization({
+	name = "General Zarithrian"
+})
+
+L:SetWarningLocalization({
+	WarnAdds	= "Neue Adds",
+	warnCleaveArmor	= "%s auf >%s< (%s)"	-- Cleave Armor on >args.destName< (args.amount)
+})
+
+L:SetTimerLocalization({
+	TimerAdds	= "Neue Adds"
+})
+
+L:SetOptionLocalization({
+	WarnAdds		= "Verkünde neue Adds",
+	TimerAdds		= "Zeige Timer für neue Adds"
+	AddsArrive		= "Show timer for adds arrival", --Needs Translating
+	warnCleaveArmor	= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(74367)
+})
+
+L:SetMiscLocalization({
+	SummonMinions	= "Äschert sie ein, Lakaien!"
+})
+
+-------------------------------------
+--  Halion the Twilight Destroyer  --
+-------------------------------------
+L = DBM:GetModLocalization("Halion")
+
+L:SetGeneralLocalization({
+	name = "Halion der Zwielichtzerstörer"
+})
+
+L:SetWarningLocalization({
+	TwilightCutterCast	= "Wirkt Zwielichtschnitter: 5 sec"
+})
+
+L:SetOptionLocalization({
+	TwilightCutterCast		= "Zeige Warnung, wenn $spell:74769 gewirkt wird",
+	AnnounceAlternatePhase	= "Zeige auch Warnungen/Timer für Phasen, in denen du dich nicht befindest",
+	SetIconOnConsumption	= "Setze Zeichen auf Ziele von Einäschern"--So we can use single functions for both versions of spell."--So we can use single functions for both versions of spell.
+})
+
+L:SetMiscLocalization({
+	Halion					= "Halion",
+	MeteorCast				= "Die Himmel brennen!",
+	Phase2					= "Ihr werdet im Reich des Zwielichts nur Leid finden! Tretet ein, wenn ihr es wagt!",
+	Phase3					= "Ich bin das Licht und die Dunkelheit! Zittert, Sterbliche, vor dem Herold Todesschwinges!",
+	twilightcutter			= "Die kreisenden Sphären pulsieren vor dunkler Energie!",
+	Kill					= "Genießt euren Sieg, Sterbliche, denn es war euer letzter. Bei der Rückkehr des Meisters wird diese Welt brennen!"
+})
