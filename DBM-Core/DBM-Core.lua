04b2d005a86fdbe59da2680f853c805cf20c16d0
Zidras <10605951+Zidras@users.noreply.github.com>
2021-08-19 22:32:42 +0100

    Core: fix UNIT event filtering
    Mod registered unit events that had args (like boss1 boss2 boss3 etc) were being sequentially shortened during handling based on the previous mod registered unit event if it also had matching args.
    To circumvent this, a current encounter variable (currentModId) was added to StartCombat, and nil'd on EndCombat. This variable is now being check on the event handler to ensure only the specific mod events that were registered on the boss lua are handled.
    
    Thanks to Kader and Barsoom for their time and support on this fix.
    
    Co-Authored-By: Kader B <bkader@mail.com>

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -709,17 +709,25 @@ do
 		return DBM:GetCIDFromGUID(self.destGUID)
 	end
 
-	local function handleEvent(self, event, ...)
-		if not registeredEvents[event] or DBM.Options and not DBM.Options.Enabled then return end
-		if event:sub(0, 5) == "UNIT_" and event:sub(event:len() - 10) ~= "_UNFILTERED" and event ~= "UNIT_DIED" and event ~= "UNIT_DESTROYED" then
-			isUnitEvent = true
+	local function IsUnitEvent(event, ...)
+		local isUnitEvent = (event and event:sub(0, 5) == "UNIT_" and event:sub(event:len() - 10) ~= "_UNFILTERED")
+		if isUnitEvent and ... and tContains({...}, event) then
+			isUnitEvent = false
 		end
+		return isUnitEvent
+	end
 
-		for i, v in ipairs(registeredEvents[event]) do
-			-- Workaround for retail-like mod:RegisterEvents("UNIT_SPELLCAST_START boss1"). Check if we have valid units registered and filter out everything else.
-			-- v is mod here... so we check registered mods for registered events with registered uIds (v.registeredUnitEvents[event]).
-			-- then we check if we have our unit (arg1) in the table ... self.registeredUnitEvents[event] = {arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8} which is defined below
-			if isUnitEvent and v and v.registeredUnitEvents and v.registeredUnitEvents[event] and not has_value(v.registeredUnitEvents[event], arg1) then return end
+    local function handleEvent(self, event, ...)
+		if not registeredEvents[event] or DBM.Options and not DBM.Options.Enabled then return end
+		local isUnitEvent = IsUnitEvent(event, "UNIT_DIED", "UNIT_DESTROYED")
+
+		for _, v in ipairs(registeredEvents[event]) do
+			if isUnitEvent and v.id == DBM.currentModId then
+				-- Workaround for retail-like mod:RegisterEvents("UNIT_SPELLCAST_START boss1"). Check if we have valid units registered and filter out everything else.
+				-- v is mod here... so we check registered mods for registered events with registered uIds (v.registeredUnitEvents[event]).
+				-- then we check if we have our unit (args) in the table ... self.registeredUnitEvents[event] = args which is defined below
+				if v.registeredUnitEvents and v.registeredUnitEvents[event] and not has_value(v.registeredUnitEvents[event], ...) then return end
+			end
 
 			if type(v[event]) == "function" and (not v.zones or checkEntry(v.zones, GetRealZoneText()) or checkEntry(v.zones, GetCurrentMapAreaID())) and (not v.Options or v.Options.Enabled) then
 				v[event](v, ...)
@@ -732,12 +740,13 @@ do
 			local event = select(i, ...)
 
 			if event:sub(0, 5) == "UNIT_" and event:sub(event:len() - 10) ~= "_UNFILTERED" and event ~= "UNIT_DIED" and event ~= "UNIT_DESTROYED" then
-				event, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8 = strsplit(" ", event)
+				local args = {strsplit(" ", event)}
+				event = tremove(args, 1)
 				-- Register valid units for retail-like mod:RegisterEvents("UNIT_SPELLCAST_START boss1")
 				-- separate units with spaces up to 9 units. Otherwise skip unit filter for that event
-				if arg1 or arg2 or arg3 or arg4 or arg5 or arg6 or arg7 or arg8 then
+				if next(args) then
 					self.registeredUnitEvents = self.registeredUnitEvents or {}
-					self.registeredUnitEvents[event] = {arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8}
+					self.registeredUnitEvents[event] = args
 				end
 			end
 
@@ -4606,6 +4615,7 @@ do
 			else
 				self:Debug("StartCombat called by individual mod or unknown reason. LastInstanceMapID is "..tostring(LastInstanceMapID))
 			end
+			self.currentModId = mod.id
 			--check completed. starting combat
 			tinsert(inCombat, mod)
 			if mod.inCombatOnlyEvents and not mod.inCombatOnlyEventsRegistered then
@@ -4783,6 +4793,7 @@ do
 
 	function DBM:EndCombat(mod, wipe, srmIncluded)
 		if removeEntry(inCombat, mod) then
+			self.currentModId = nil
 			if mod.updateInterval then
 				mod:UnregisterOnUpdateHandler()
 			end
