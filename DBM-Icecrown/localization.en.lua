7f82e3969216c93067103b71d8efa6ab53053eb1
Zidras <10605951+Zidras@users.noreply.github.com>
2021-08-22 18:41:11 +0100

    ICCTrash: fix EN locale typo

--- DBM-Icecrown/localization.en.lua
+++ DBM-Icecrown/localization.en.lua
@@ -392,7 +392,7 @@ L:SetWarningLocalization{
 
 L:SetOptionLocalization{
 	SpecWarnTrapL		= "Show special warning for Deathbound Ward trap activation",
-	SpecWarnTrapP		= "Show special warning for engeful Fleshreapers trap activation",
+	SpecWarnTrapP		= "Show special warning for Vengeful Fleshreapers trap activation",
 	SpecWarnGosaEvent	= "Show special warning for Sindragosa gauntlet event"
 }
 
