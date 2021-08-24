dafeb0775651dc75f903c25855dddb416759391b
Nispodzianka <fantomer@gmail.com>
2021-08-24 15:58:32 +0200

    fix RemoveBuffs + timers + fearsoon

--- DBM-ChamberOfAspects/Ruby/Zarithrian.lua
+++ DBM-ChamberOfAspects/Ruby/Zarithrian.lua
@@ -20,29 +20,34 @@ local warnCleaveArmor			= mod:NewStackAnnounce(74367, 2, nil, "Tank|Healer")
 local specWarnFear				= mod:NewSpecialWarningSpell(74384, nil, nil, nil, 2, 2)
 local specWarnCleaveArmor		= mod:NewSpecialWarningStack(74367, nil, 2, nil, nil, 1, 6)--ability lasts 30 seconds, has a 15 second cd, so tanks should trade at 2 stacks.
 
-local timerAddsCD				= mod:NewTimer(45.5, "TimerAdds", 74398, nil, nil, 1)
+local timerAddsCD				= mod:NewTimer(40, "TimerAdds", 74398, nil, nil, 1)
 local timerAddsTravel			= mod:NewTimer(10, "AddsArrive") -- Timer to indicate when the summoned adds arive
 local timerCleaveArmor			= mod:NewTargetTimer(30, 74367, nil, "Tank|Healer", nil, 5)
-local timerFearCD				= mod:NewCDTimer(37, 74384, nil, nil, nil, 2)--anywhere from 35-40 seconds in between fears.
+local timerFearCD				= mod:NewCDTimer(30, 74384, nil, nil, nil, 2)
 
 mod:AddBoolOption("CancelBuff")
 
+function mod:RemoveBuffs()
+	CancelUnitBuff("player", (GetSpellInfo(10278)))		-- Hand of Protection
+	CancelUnitBuff("player", (GetSpellInfo(642)))		-- Divine Shield
+end
+
 function mod:SPELL_AURA_REMOVED(args)
 	if args:IsSpellID(74367) and self.Options.CancelBuff then
-		CancelUnitBuff("player", (GetSpellInfo(10278)))		-- Hand of Protection
-		CancelUnitBuff("player", (GetSpellInfo(642)))		-- Divine Shield
+		self:ScheduleMethod(0.01, "RemoveBuffs")
 	end
 end
 
 function mod:OnCombatStart(delay)
-	timerFearCD:Start(14-delay)--need more pulls to verify consistency
-	timerAddsCD:Start(15.5-delay)--need more pulls to verify consistency
+	timerFearCD:Start(14-delay)
+	specWarnFear:ScheduleVoice(11, "fearsoon") -- 3 secs prewarning
+	timerAddsCD:Start(18-delay)
 end
 
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 74384 then
 		specWarnFear:Show()
-		specWarnFear:Play("fearsoon")
+		specWarnFear:ScheduleVoice(27, "fearsoon") -- 3 secs prewarning
 		timerFearCD:Start()
 	end
 end
