7b6b1356478aeb79a048c6900a66ad22bd9852ef
Zidras <10605951+Zidras@users.noreply.github.com>
2021-11-21 13:40:33 +0000

    Valithria: Change registerCombat method to "combat"
    The yell on pull is currently bugged on warmane. https://www.warmane.com/bugtracker/report/105904
    
    For this reason, combat is now to be detected via IEEU.

--- DBM-Icecrown/FrostwingHalls/Valithria.lua
+++ DBM-Icecrown/FrostwingHalls/Valithria.lua
@@ -6,7 +6,7 @@ mod:SetCreatureID(36789)
 mod:SetUsedIcons(8)
 mod.onlyHighest = true--Instructs DBM health tracking to literally only store highest value seen during fight, even if it drops below that
 
-mod:RegisterCombat("yell", L.YellPull)
+mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
 	"SPELL_CAST_START",
