8580ef0cd065fcfce709d7f0959065ff33815045
Zidras <10605951+Zidras@users.noreply.github.com>
2021-12-28 23:05:47 +0000

    EoE: Update TOC and add missing locales

---
+++ DBM-EyeOfEternity/localization.fr.lua
@@ -0,0 +1,21 @@
+if GetLocale() ~= "frFR" then return end
+
+local L
+
+---------------
+--  Malygos  --
+---------------
+L = DBM:GetModLocalization("Malygos")
+
+L:SetGeneralLocalization({
+	name = "Malygos"
+})
+
+L:SetMiscLocalization({
+	YellPull		= "Ma patience a ses limites. Je vais me débarrasser de vous !",
+	EmoteSpark		= "de puissance surgit",
+	YellPhase2		= "Je pensais mettre rapidement fin à vos existences",
+	YellBreath		= "Vous n'arriverez à rien tant qu'il me restera un souffle !",
+	YellPhase3		= "Vos bienfaiteurs font enfin leur entrée, mais ils arrivent trop tard !"
+})
+
