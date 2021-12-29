672d167cef014f80b849a3bad23c3c8e38286520
Zidras <10605951+Zidras@users.noreply.github.com>
2021-12-29 17:41:30 +0000

    VoidReaver: Update to retail code structure

--- DBM-TheEye/VoidReaver.lua
+++ DBM-TheEye/VoidReaver.lua
@@ -4,23 +4,21 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(19516)
 
-mod:SetZone()
-
 mod:RegisterCombat("combat")
 
-mod:RegisterEvents(
-	"SPELL_CAST_SUCCESS"
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_SUCCESS 34172 34162 25778"
 )
 
-local warnOrb			= mod:NewTargetAnnounce(34172, 2, nil, false)
+local warnOrb			= mod:NewTargetNoFilterAnnounce(34172, 2)
 local warnKnockBack		= mod:NewSpellAnnounce(25778, 4)
 local warnPounding		= mod:NewSpellAnnounce(34162, 3)
 
-local specWarnOrb		= mod:NewSpecialWarningMove(34172,false)
---local yellOrb			= mod:NewYellMe(34172,false)
+local specWarnOrb		= mod:NewSpecialWarningDodge(34172, nil, nil, nil, 1, 2)
+local yellOrb			= mod:NewYell(34172)
 
-local timerKnockBack	= mod:NewCDTimer(20, 25778)
-local timerPounding		= mod:NewCDTimer(13, 34162)
+local timerKnockBack	= mod:NewCDTimer(20, 25778, nil, "Tank", 2, 5)
+local timerPounding		= mod:NewCDTimer(13, 34162, nil, nil, nil, 2)
 
 local berserkTimer		= mod:NewBerserkTimer(600)
 
@@ -31,10 +29,12 @@ end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args.spellId == 34172 then
-		warnOrb:Show(args.destName)
 		if args:IsPlayer() then
 			specWarnOrb:Show()
-			--yellOrb:Yell()
+			specWarnOrb:Play("watchorb")
+			yellOrb:Yell()
+		else
+			warnOrb:Show(args.destName)
 		end
 	elseif args.spellId == 34162 then
 		warnPounding:Show()
