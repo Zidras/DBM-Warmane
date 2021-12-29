6b79da76d3bdc8dddb238172af922a44dba4d4a8
Zidras <10605951+Zidras@users.noreply.github.com>
2021-12-29 00:15:03 +0000

    Shadron: Update to retail code structure

--- DBM-ChamberOfAspects/Obsidian/Shadron.lua
+++ DBM-ChamberOfAspects/Obsidian/Shadron.lua
@@ -3,21 +3,20 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 3695 $"):sub(12, -3))
 mod:SetCreatureID(30451)
-mod:SetZone()
 
 mod:RegisterCombat("combat")
 
-mod:RegisterEvents(
-    "SPELL_CAST_SUCCESS"
+mod:RegisterEventsInCombat(
+    "SPELL_CAST_SUCCESS 57579 59127"
 )
 
-local warnShadowFissure	= mod:NewSpellAnnounce(59127)
-local timerShadowFissure = mod:NewCastTimer(5, 59128)--Cast timer until Void Blast. it's what happens when shadow fissure explodes.
-
+local warnShadowFissure		= mod:NewSpellAnnounce(59127, nil, nil, nil, nil, nil, 2)
+local timerShadowFissure	= mod:NewCastTimer(5, 59128, nil, nil, nil, 3)--Cast timer until Void Blast. it's what happens when shadow fissure explodes.
 
 function mod:SPELL_CAST_SUCCESS(args)
-    if args:IsSpellID(57579, 59127) and self:IsInCombat() then
+    if args:IsSpellID(57579, 59127) then
         warnShadowFissure:Show()
+        warnShadowFissure:Play("watchstep")
         timerShadowFissure:Start()
     end
 end
