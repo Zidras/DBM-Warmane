bcc7360ad0e2df0268b88e2301634b558b240847
Zidras <10605951+Zidras@users.noreply.github.com>
2021-12-29 10:41:01 +0000

    RageWinterchill: Update to retail code structure

--- DBM-Hyjal/RageWinterchill.lua
+++ DBM-Hyjal/RageWinterchill.lua
@@ -4,29 +4,28 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(17767)
 mod:SetModelID("creature/lich/lich.m2")
-mod:SetZone()
 mod:SetUsedIcons(8)
 
 mod:RegisterCombat("combat")
 
-mod:RegisterEvents(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START"
+mod:RegisterEventsInCombat(
+	"SPELL_AURA_APPLIED 31249 31258",
+	"SPELL_AURA_REMOVED 31249",
+	"SPELL_CAST_START 31258"
 )
 
 local warnIceBolt		= mod:NewSpellAnnounce(31249, 3)
 local warnDnd			= mod:NewSpellAnnounce(31258, 3)
 
-local timerDnd			= mod:NewBuffActiveTimer(16, 31258)
-local timerDndCD		= mod:NewCDTimer(46, 31258)
+local specWarnIceBolt	= mod:NewSpecialWarningYou(31249, nil, nil, nil, 1, 2)
+local specWarnDnD		= mod:NewSpecialWarningGTFO(31258, nil, nil, nil, 1, 8)
 
-local specWarnIceBolt	= mod:NewSpecialWarningYou(31249)
-local specWarnDnD		= mod:NewSpecialWarningMove(31258)
+local timerDnd			= mod:NewBuffActiveTimer(16, 31258)
+local timerDndCD		= mod:NewCDTimer(46, 31258, nil, nil, nil, 3)
 
 local berserkTimer		= mod:NewBerserkTimer(600)
 
-mod:AddBoolOption("IceBoltIcon", false)
+mod:AddSetIconOption("IceBoltIcon", 31249, false, false, {8})
 
 function mod:OnCombatStart(delay)
 	berserkTimer:Start(-delay)
@@ -34,15 +33,18 @@ end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 31249 then
-		warnIceBolt:Show(args.destName)
 		if args:IsPlayer() then
 			specWarnIceBolt:Show()
+			specWarnIceBolt:Play("stunsoon")
+		else
+			warnIceBolt:Show(args.destName)
 		end
 		if self.Options.IceBoltIcon then
 			self:SetIcon(args.destName, 8)
 		end
 	elseif args.spellId == 31258 and args:IsPlayer() and self:AntiSpam() then
-		specWarnDnD:Show()
+		specWarnDnD:Show(args.spellName)
+		specWarnDnD:Play("watchfeet")
 	end
 end
 
