cd2d64d94f3c73e541e73e25d15cf20688ad571e
Zidras <10605951+Zidras@users.noreply.github.com>
2021-12-29 19:19:11 +0000

    Anh'Kahet: Update to retail code structure

--- DBM-Party-WotLK/AhnKahet/Taldaram.lua
+++ DBM-Party-WotLK/AhnKahet/Taldaram.lua
@@ -1,27 +1,28 @@
 local mod	= DBM:NewMod("Taldaram", "DBM-Party-WotLK", 1)
 local L		= mod:GetLocalizedStrings()
 
+mod.statTypes = "normal,heroic"
+
 mod:SetRevision(("$Revision: 2250 $"):sub(12, -3))
 mod:SetCreatureID(29308)
-mod:SetZone()
 
 mod:RegisterCombat("combat")
 
-mod:RegisterEvents(
-	"SPELL_CAST_START",
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REMOVED"
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_START 55931",
+	"SPELL_AURA_APPLIED 55959 59513",
+	"SPELL_AURA_REMOVED 55959 59513"
 )
 
 local warningEmbrace	= mod:NewTargetAnnounce(55959, 2)
 local warningFlame		= mod:NewSpellAnnounce(55931, 3)
 
-local timerEmbrace		= mod:NewTargetTimer(20, 55959)
-local timerFlameCD		= mod:NewCDTimer(17, 55959)
+local timerEmbrace		= mod:NewTargetTimer(20, 55959, nil, nil, nil, 3, nil, DBM_CORE_L.DAMAGE_ICON)
+local timerFlameCD		= mod:NewCDTimer(15, 55931, nil, nil, nil, 3)
 
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(55931) then
+	if args.spellId == 55931 then
 		warningFlame:Show()
 		timerFlameCD:Start()
 	end
