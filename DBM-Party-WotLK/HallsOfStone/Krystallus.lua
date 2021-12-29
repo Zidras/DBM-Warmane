bd9ca49a3d0361ceffdf20acdd9f93eb56352099
Zidras <10605951+Zidras@users.noreply.github.com>
2021-12-29 23:38:28 +0000

    HallsOfStone: Update to retail code structure

--- DBM-Party-WotLK/HallsOfStone/Krystallus.lua
+++ DBM-Party-WotLK/HallsOfStone/Krystallus.lua
@@ -3,19 +3,19 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 2250 $"):sub(12, -3))
 mod:SetCreatureID(27977)
-mod:SetZone()
 
 mod:RegisterCombat("combat")
 
-local warningShatter	= mod:NewSpellAnnounce(50810, 3)
-local timerShatterCD	= mod:NewCDTimer(25, 50810)
-
-mod:RegisterEvents(
-	"SPELL_CAST_SUCCESS"
+mod:RegisterEventsInCombat(
+	"SPELL_CAST_SUCCESS 50833"
 )
 
+local warningShatter	= mod:NewSpellAnnounce(50810, 3)
+
+local timerShatterCD	= mod:NewCDTimer(25, 50810, nil, nil, nil, 2)
+
 function mod:SPELL_CAST_SUCCESS(args)
-	if args:IsSpellID(50833) then
+	if args.spellId == 50833 then
 		warningShatter:Show()	-- Shatter warning when Ground Slam is cast
 		timerShatterCD:Start()
 	end
--- DBM-Party-WotLK/HallsOfStone/SjonnirTheIronshaper.lua
+++ DBM-Party-WotLK/HallsOfStone/SjonnirTheIronshaper.lua
@@ -3,26 +3,35 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision(("$Revision: 2250 $"):sub(12, -3))
 mod:SetCreatureID(27978)
-mod:SetZone()
 
 mod:RegisterCombat("combat")
 
+mod:RegisterEventsInCombat(
+	"SPELL_AURA_APPLIED 59848 50840 59861 51849 50834 59846"
+)
+
 local warningCharge		= mod:NewTargetAnnounce(50834, 2)
 local warningRing		= mod:NewSpellAnnounce(50840, 3)
-local timerCharge		= mod:NewTargetTimer(10, 50834)
-local timerChargeCD		= mod:NewCDTimer(25, 50834)
-local timerRingCD		= mod:NewCDTimer(25, 50840)
 
-mod:RegisterEvents(
-	"SPELL_AURA_APPLIED"
-)
+local specWarnCharge	= mod:NewSpecialWarningMoveAway(50834, nil, nil, nil, 1, 2)
+local yellCharge		= mod:NewYell(50834)
+
+local timerCharge		= mod:NewTargetTimer(10, 50834)
+local timerChargeCD		= mod:NewCDTimer(25, 50834, nil, nil, nil, 3)
+local timerRingCD		= mod:NewCDTimer(25, 50840, nil, nil, nil, 2)
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(59848, 50840, 59861, 51849) then
 		warningRing:Show()
 		timerRingCD:Start()
 	elseif args:IsSpellID(50834, 59846) then
-		warningCharge:Show(args.destName)
+		if args:IsPlayer() then
+			specWarnCharge:Show()
+			specWarnCharge:Play("runout")
+			yellCharge:Yell()
+		else
+			warningCharge:Show(args.destName)
+		end
 		timerCharge:Start(args.destName)
 		timerChargeCD:Start()
 	end
