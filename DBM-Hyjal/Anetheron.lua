247954fdaa9c60bfa832ee23d3be61c65e8f5d3d
Zidras <10605951+Zidras@users.noreply.github.com>
2021-12-29 11:07:08 +0000

    Anetheron: Update to retail code structure

--- DBM-Hyjal/Anetheron.lua
+++ DBM-Hyjal/Anetheron.lua
@@ -4,35 +4,36 @@ local L		= mod:GetLocalizedStrings()
 mod:SetRevision(("$Revision$"):sub(12, -3))
 mod:SetCreatureID(17808)
 
-mod:SetZone()
-
 mod:RegisterCombat("combat")
 
-mod:RegisterEvents(
-	"SPELL_AURA_APPLIED",
-	"SPELL_AURA_REFRESH",
-	"SPELL_AURA_REMOVED",
-	"SPELL_CAST_START",
-	"SPELL_CAST_SUCCESS"
+mod:RegisterEventsInCombat(
+	"SPELL_AURA_APPLIED 31306 31298",
+	"SPELL_AURA_REFRESH 31306 31298",
+	"SPELL_AURA_REMOVED 31306 31298",
+	"SPELL_CAST_START 31299",
+	"SPELL_CAST_SUCCESS 31306 31298"
 )
 
 local warnSwarm			= mod:NewSpellAnnounce(31306, 3)
-local warnSleep			= mod:NewTargetAnnounce(31298, 2)
-local warnInferno		= mod:NewTargetAnnounce(31299, 4)
+local warnSleep			= mod:NewTargetNoFilterAnnounce(31298, 2)
+local warnInferno		= mod:NewTargetNoFilterAnnounce(31299, 4)
 
-local timerSwarm		= mod:NewBuffFadesTimer(20, 31306)
-local timerSleepCD		= mod:NewCDTimer(19, 31298)
-local timerInferno		= mod:NewCDTimer(51, 31299)
+local specWarnInferno	= mod:NewSpecialWarningYou(31299, nil, nil, nil, 1, 2)
+local yellInferno		= mod:NewYell(31299)
 
-local specWarnInferno	= mod:NewSpecialWarningYou(31299)
+local timerSwarm		= mod:NewBuffFadesTimer(20, 31306, nil, nil, nil, 3)
+local timerSleep		= mod:NewBuffFadesTimer(10, 31298, nil, nil, nil, 3)
+local timerSleepCD		= mod:NewCDTimer(19, 31298, nil, nil, nil, 3)
+local timerInferno		= mod:NewCDTimer(51, 31299, nil, nil, nil, 3)
 
-function mod:InfernoTarget()
-	local targetname = self:GetBossTarget(17808)
+function mod:InfernoTarget(targetname, uId)
 	if not targetname then return end
-	warnInferno:Show(targetname)
-	timerInferno:Start()
 	if targetname == UnitName("player") then
 		specWarnInferno:Show()
+		specWarnInferno:Play("targetyou")
+		yellInferno:Yell()
+	else
+		warnInferno:Show(targetname)
 	end
 end
 
@@ -40,7 +41,7 @@ function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 31306 and args:IsPlayer() then
 		timerSwarm:Start()
 	elseif args.spellId == 31298 and args:IsPlayer() then
-		timerSleepCD:Start()
+		timerSleep:Start()
 	end
 end
 mod.SPELL_AURA_REFRESH = mod.SPELL_AURA_APPLIED
@@ -49,13 +50,14 @@ function mod:SPELL_AURA_REMOVED(args)
 	if args.spellId == 31306 and args:IsPlayer() then
 		timerSwarm:Cancel()
 	elseif args.spellId == 31298 and args:IsPlayer() then
-		timerSleepCD:Cancel()
+		timerSleep:Cancel()
 	end
 end
 
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 31299 then
-		self:ScheduleMethod(0.15, "InfernoTarget")
+		timerInferno:Start()
+		self:BossTargetScanner(17808, "InfernoTarget", 0.05, 10)
 	end
 end
 
@@ -66,4 +68,3 @@ function mod:SPELL_CAST_SUCCESS(args)
 		timerSleepCD:Start()
 	end
 end
-
