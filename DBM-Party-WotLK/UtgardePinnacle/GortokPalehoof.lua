711a68a697aa5c7dfe08a801c6c8f7a494d3c5e0
Zidras <10605951+Zidras@users.noreply.github.com>
2021-12-30 00:47:44 +0000

    UtgardePinnacle: Update to retail code structure

--- DBM-Party-WotLK/UtgardePinnacle/GortokPalehoof.lua
+++ DBM-Party-WotLK/UtgardePinnacle/GortokPalehoof.lua
@@ -1,18 +1,20 @@
 local mod	= DBM:NewMod("GortokPalehoof", "DBM-Party-WotLK", 11)
 local L		= mod:GetLocalizedStrings()
 
+mod.statTypes = "normal,heroic"
+
 mod:SetRevision(("$Revision: 2250 $"):sub(12, -3))
 mod:SetCreatureID(26687)
-mod:SetZone()
 
 mod:RegisterCombat("combat")
 
-mod:RegisterEvents(
-	"SPELL_AURA_APPLIED"
+mod:RegisterEventsInCombat(
+	"SPELL_AURA_APPLIED 48261 59268"
 )
 
-local warningImpale		= mod:NewTargetAnnounce(48261, 2)
-local timerImpale		= mod:NewTargetTimer(9, 48261)
+local warningImpale		= mod:NewTargetNoFilterAnnounce(48261, 2, nil, "Healer")
+
+local timerImpale		= mod:NewTargetTimer(9, 48261, nil, "Healer", 2, 5, nil, DBM_CORE_L.HEALER_ICON)
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(48261, 59268) then
--- DBM-Party-WotLK/UtgardePinnacle/SvalaSorrowgrave.lua
+++ DBM-Party-WotLK/UtgardePinnacle/SvalaSorrowgrave.lua
@@ -1,26 +1,38 @@
 local mod	= DBM:NewMod("SvalaSorrowgrave", "DBM-Party-WotLK", 11)
 local L		= mod:GetLocalizedStrings()
 
+mod.statTypes = "normal,heroic"
+
 mod:SetRevision(("$Revision: 4439 $"):sub(12, -3))
 mod:SetCreatureID(26668)
-mod:SetZone()
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEvents(
-	"SPELL_AURA_APPLIED",
 	"CHAT_MSG_MONSTER_YELL"
 )
 
-local warningSacrifice	= mod:NewTargetAnnounce(48267, 2)
-local timerSacrifice	= mod:NewBuffActiveTimer(25, 48276)
-local timerRoleplay		= mod:NewTimer(67, "timerRoleplay", "Interface\\Icons\\Spell_Holy_BorrowedTime")--roleplay for boss is active
+mod:RegisterEventsInCombat(
+	"SPELL_AURA_APPLIED 48267 48276",
+	"SPELL_AURA_REMOVED 48276"
+)
+
+local warningSacrifice	= mod:NewTargetNoFilterAnnounce(48267, 4)
+
+local timerSacrifice	= mod:NewBuffActiveTimer(25, 48276, nil, nil, nil, 5, nil, DBM_CORE_L.DAMAGE_ICON)
+local timerRoleplay		= mod:NewTimer(67, "timerRoleplay", "237538")--roleplay for boss is active
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args:IsSpellID(48267) then
+	if args.spellId == 48267 then
 		warningSacrifice:Show(args.destName)
-	elseif args:IsSpellID(48276) then
+	elseif args.spellId == 48276 then
 		timerSacrifice:Start()
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	if args.spellId == 48276 then
+		timerSacrifice:Stop()
 	end
 end
 
