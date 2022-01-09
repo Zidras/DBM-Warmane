bc1ce329553e7750dedb6023d9d0c981036f9f5f
Nispodzianka <87914582+Nispodzianka@users.noreply.github.com>
2022-01-09 17:59:24 +0100

    undoing changes

--- DBM-Icecrown/localization.cn.lua
+++ DBM-Icecrown/localization.cn.lua
@@ -236,6 +236,7 @@ L:SetTimerLocalization{
     TimerPortalsOpen = "传送门开启",
     TimerPortalsClose = "Portals close", --Needs translating
     TimerBlazingSkeleton = "下一次炽热骷髅",
+    TimerAbom = "下一次憎恶体"
 }
 
 L:SetOptionLocalization{
@@ -244,6 +245,7 @@ L:SetOptionLocalization{
     TimerPortalsOpen = "当梦魇之门开启时显示计时器",
     TimerPortalsClose = "Show timer when Nightmare Portals are closed", --Needs translating
     TimerBlazingSkeleton = "为下一次炽热骷髅出现显示计时器",
+    TimerAbom = "为下一次贪吃的憎恶体出现显示计时器"
 }
 
 L:SetMiscLocalization{
