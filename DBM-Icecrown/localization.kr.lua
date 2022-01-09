151cfa462b01989726ae332166601bea1627cbed
Nispodzianka <87914582+Nispodzianka@users.noreply.github.com>
2022-01-09 18:05:19 +0100

    undoing changes

--- DBM-Icecrown/localization.kr.lua
+++ DBM-Icecrown/localization.kr.lua
@@ -1,4 +1,4 @@
-﻿if GetLocale() ~= "koKR" then return end
+if GetLocale() ~= "koKR" then return end
 
 local L
 
@@ -239,6 +239,7 @@ L:SetTimerLocalization{
 	TimerPortalsOpen			= "차원문 열림",
 	TimerPortalsClose			= "차원문 닫힘",
 	TimerBlazingSkeleton		= "다음 타오르는 해골",
+	TimerAbom					= "다음 누더기골렘"
 }
 
 L:SetOptionLocalization{
@@ -247,6 +248,7 @@ L:SetOptionLocalization{
 	TimerPortalsOpen			= "차원문 열림 바 보기",
 	TimerPortalsClose			= "차원문 닫힘 바 보기",
 	TimerBlazingSkeleton		= "다음 타오르는 해골 바 보기",
+	TimerAbom					= "다음 걸신들린 누더기골렘 바 보기(부정확할 수 있음)"
 }
 
 L:SetMiscLocalization{
