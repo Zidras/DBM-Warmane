d33b729df9b54412f29dcb85f5b802ae241e7711
Nispodzianka <87914582+Nispodzianka@users.noreply.github.com>
2022-01-25 02:02:43 +0100

    fix warningLeech and add specWarningCloud
    code for specWarningCloud taken from Gundrak-BloodstoneAnnihilator.lua

--- DBM-Party-WotLK/AzjolNerub/Hadronox.lua
+++ DBM-Party-WotLK/AzjolNerub/Hadronox.lua
@@ -7,16 +7,22 @@ mod:SetCreatureID(28921)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 53030 59417 53400 59419"
+	"SPELL_AURA_APPLIED 53030 59417",
+    "SPELL_PERIODIC_DAMAGE 53400 59419"
 )
 
-local warningCloud	= mod:NewSpellAnnounce(53400, 3)
+local specWarningCloud	= mod:NewSpecialWarningMove(53400, nil, nil, nil, 1, 2)
 local warningLeech	= mod:NewSpellAnnounce(53030, 1)
 
-function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(53030, 59417) then
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(53030, 59417) and args:IsPlayer() then
 		warningLeech:Show()
-	elseif args:IsSpellID(53400, 59419) then
-		warningCloud:Show()
+	end
+end
+
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, destGUID, _, _, spellId)
+ 	if (spellId == 53400 or spellId == 59419) and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) and not self:IsTrivial() then
+ 		specWarningCloud:Show()
+ 		specWarningCloud:Play("runaway")
 	end
 end
