2d71731badebcdc0a8e9751684ea257da3499b03
Nispodzianka <87914582+Nispodzianka@users.noreply.github.com>
2022-01-31 02:27:19 +0100

    add SPELL_PERIODIC_MISSED

--- DBM-Party-WotLK/AzjolNerub/Hadronox.lua
+++ DBM-Party-WotLK/AzjolNerub/Hadronox.lua
@@ -8,10 +8,11 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 53030 59417",
-	"SPELL_PERIODIC_DAMAGE 53400 59419"
+	"SPELL_PERIODIC_DAMAGE 53400 59419",
+	"SPELL_PERIODIC_MISSED 53400 59419"
 )
 
-local specWarningCloud	= mod:NewSpecialWarningMove(53400, nil, nil, nil, 1, 2)
+local specWarningCloudGTFO	= mod:NewSpecialWarningMove(53400, nil, nil, 2, 4, 2)
 local warningLeech	= mod:NewSpellAnnounce(53030, 1)
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -21,8 +22,9 @@ function mod:SPELL_AURA_APPLIED(args)
 end
 
 function mod:SPELL_PERIODIC_DAMAGE(_, _, _, destGUID, _, _, spellId)
-	if (spellId == 53400 or spellId == 59419) and destGUID == UnitGUID("player") and self:AntiSpam(2, 1) and not self:IsTrivial() then
-		specWarningCloud:Show()
-		specWarningCloud:Play("runaway")
+	if (spellId == 53400 or spellId == 59419) and destGUID == UnitGUID("player") and self:AntiSpam(3, 1) then
+		specWarningCloudGTFO:Show()
+		specWarningCloudGTFO:Play("runaway")
 	end
 end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
