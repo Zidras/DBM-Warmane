de486e1e54a4d0906bd9b6f976000bce1eeb1b61
Nispodzianka <87914582+Nispodzianka@users.noreply.github.com>
2022-02-02 01:47:22 +0100

    AzjolNerub: rework Krikthir and Hadronox warnings (#105)
    Co-authored-by: Zidras <10605951+Zidras@users.noreply.github.com>

--- DBM-Party-WotLK/AzjolNerub/Hadronox.lua
+++ DBM-Party-WotLK/AzjolNerub/Hadronox.lua
@@ -7,16 +7,25 @@ mod:SetCreatureID(28921)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 53030 59417 53400 59419"
+	"SPELL_AURA_APPLIED 53030 59417",
+	"SPELL_PERIODIC_DAMAGE 53400 59419",
+	"SPELL_PERIODIC_MISSED 53400 59419"
 )
 
-local warningCloud	= mod:NewSpellAnnounce(53400, 3)
 local warningLeech	= mod:NewSpellAnnounce(53030, 1)
 
-function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(53030, 59417) then
+local specWarnGTFO	= mod:NewSpecialWarningGTFO(53400, nil, nil, nil, 1, 8)
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(53030, 59417) and args:IsPlayer() then
 		warningLeech:Show()
-	elseif args:IsSpellID(53400, 59419) then
-		warningCloud:Show()
 	end
 end
+
+function mod:SPELL_PERIODIC_DAMAGE(_, _, _, destGUID, _, _, spellId, spellName)
+	if (spellId == 53400 or spellId == 59419) and destGUID == UnitGUID("player") and self:AntiSpam(3, 1) then
+		specWarnGTFO:Show(spellName)
+		specWarnGTFO:Play("watchfeet")
+	end
+end
+mod.SPELL_PERIODIC_MISSED = mod.SPELL_PERIODIC_DAMAGE
--- DBM-Party-WotLK/AzjolNerub/Krikthir.lua
+++ DBM-Party-WotLK/AzjolNerub/Krikthir.lua
@@ -7,16 +7,28 @@ mod:SetCreatureID(28684)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 52592 59368"
+	"SPELL_AURA_APPLIED 52592 59368",
+	"SPELL_CAST_START 52592 59368"
 )
 
-local warningCurse	= mod:NewSpellAnnounce(52592, 2)
+local warnCurse	= mod:NewSpellAnnounce(52592, 2)
 
-local timerCurseCD	= mod:NewCDTimer(20, 52592, nil, nil, nil, 2)
+local specWarnCurse	= mod:NewSpecialWarningYou(52592, nil, nil, nil, 1, 2)
+
+local timerCurseCD	= mod:NewCDTimer(10, 52592, nil, nil, nil, 2)
+
+function mod:SPELL_CAST_START(args)
+	if args:IsSpellID(52592, 59368) then
+		warnCurse:Show()
+	end
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(52592, 59368) then
-		warningCurse:Show()
 		timerCurseCD:Start()
+		if args:IsPlayer() then
+			specWarnCurse:Show()
+			specWarnCurse:Play("targetyou")
+		end
 	end
 end
