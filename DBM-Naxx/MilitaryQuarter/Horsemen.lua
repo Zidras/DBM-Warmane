bc779899c8eee7870c5bc738a35bfc55bbe11987
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2022-03-19 13:52:46 +0000

    4 Horsemen: add Meteor timer, make timer names more consistent

--- DBM-Naxx/MilitaryQuarter/Horsemen.lua
+++ DBM-Naxx/MilitaryQuarter/Horsemen.lua
@@ -20,10 +20,11 @@ local warnMeteor				= mod:NewSpellAnnounce(57467, 4)
 local specWarnMarkOnPlayer		= mod:NewSpecialWarning("SpecialWarningMarkOnPlayer", nil, nil, nil, 1, 6)
 
 local timerLadyMark				= mod:NewNextTimer(16, 28833)
-local NextZeliekMark			= mod:NewNextTimer(16, 28835)
-local NextBaronMark				= mod:NewNextTimer(15, 28834)
-local NextThaneMark				= mod:NewNextTimer(15, 28832)
-local holyWrathCD				= mod:NewCDTimer(13, 57466)
+local timerZeliekMark			= mod:NewNextTimer(16, 28835)
+local timerBaronMark			= mod:NewNextTimer(15, 28834)
+local timerThaneMark			= mod:NewNextTimer(15, 28832)
+local timerHolyWrathCD			= mod:NewCDTimer(13, 57466)
+local timerMeteorCD				= mod:NewCDTimer(15, 57467, nil, nil, nil, 2)
 
 mod:AddRangeFrameOption("12")
 
@@ -36,13 +37,21 @@ mod:SetBossHealthInfo(
 
 mod.vb.markCounter = 0
 
+-- Still 15s timer on next meteor when he skips one (usually on tank swaps)
+local function MeteorCast(self)
+	self:Unschedule(MeteorCast)
+	timerMeteorCD:Start()
+	self:Schedule(15, MeteorCast, self)
+end
+
 function mod:OnCombatStart(delay)
 	self.vb.markCounter = 0
 	timerLadyMark:Start()
-	NextZeliekMark:Start()
-	NextBaronMark:Start()
-	NextThaneMark:Start()
+	timerZeliekMark:Start()
+	timerBaronMark:Start()
+	timerThaneMark:Start()
 	warnMarkSoon:Schedule(12)
+	timerMeteorCD:Start()
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Show(12)
 	end
@@ -57,6 +66,7 @@ end
 function mod:SPELL_CAST_START(args)
 	if args:IsSpellID(28884, 57467) then
 		warnMeteor:Show()
+		MeteorCast(self)
 	end
 end
 
@@ -67,15 +77,15 @@ function mod:SPELL_CAST_SUCCESS(args)
 		if spellId == 28833 then -- Lady Mark
 			timerLadyMark:Start(15)
 		elseif spellId == 28835 then -- Zeliek Mark
-			NextZeliekMark:Start(15)
+			timerZeliekMark:Start(15)
 		elseif spellId == 28834 then -- Barok Mark
-			NextBaronMark:Start()
+			timerBaronMark:Start()
 		elseif spellId == 28832 then -- Thane Mark
-			NextThaneMark:Start()
+			timerThaneMark:Start()
 		end
 		warnMarkSoon:Schedule(12)
 	elseif args:IsSpellID(28883, 53638, 57466, 32455) then
-		holyWrathCD:Start()
+		timerHolyWrathCD:Start()
 	end
 end
 
@@ -92,12 +102,14 @@ end
 function mod:UNIT_DIED(args)
 	local cid = self:GetCIDFromGUID(args.destGUID)
 	if cid == 16064 then
-		NextThaneMark:Cancel()
+		timerThaneMark:Cancel()
+		timerMeteorCD:Cancel()
+		self:Unschedule(MeteorCast)
 	elseif cid == 30549 then
-		NextBaronMark:Cancel()
+		timerBaronMark:Cancel()
 	elseif cid == 16065 then
 		timerLadyMark:Cancel()
 	elseif cid == 16063 then
-		NextZeliekMark:Cancel()
+		timerZeliekMark:Cancel()
 	end
 end
