181a174674cc2c1cbd57dd6c01c605aa69f3a6bd
Zidras <10605951+Zidras@users.noreply.github.com>
2022-04-02 12:03:32 +0100

    UtgardePinnacle: prevent spam of Screams of the Dead on Ymiron
    ["SPELL_CAST_SUCCESS"] = {
    	"Alaridos de los muertos-51750-npc:26861 = pull:33.9, 13.0, 16.4, 17.2", -- [1]

--- DBM-Party-WotLK/UtgardePinnacle/Ymiron.lua
+++ DBM-Party-WotLK/UtgardePinnacle/Ymiron.lua
@@ -7,7 +7,8 @@ mod:SetCreatureID(26861)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_AURA_APPLIED 48294 59301 51750",
+	"SPELL_CAST_SUCCESS 51750",
+	"SPELL_AURA_APPLIED 48294 59301",
 	"SPELL_AURA_REMOVED 48294 59301"
 )
 
@@ -17,13 +18,17 @@ local warningScreams	= mod:NewSpellAnnounce(51750, 2)
 local timerBane			= mod:NewBuffActiveTimer(5, 48294, nil, nil, nil, 5, nil, DBM_CORE_L.MAGIC_ICON)
 local timerScreams		= mod:NewBuffActiveTimer(8, 51750, nil, nil, nil, 2)
 
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 51750 then
+		warningScreams:Show()
+		timerScreams:Start()
+	end
+end
+
 function mod:SPELL_AURA_APPLIED(args)
 	if args:IsSpellID(48294, 59301) then
 		warningBane:Show()
 		timerBane:Start()
-	elseif args.spellId == 51750 then
-		warningScreams:Show()
-		timerScreams:Start()
 	end
 end
 
