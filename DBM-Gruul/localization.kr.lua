b3fe247602f3ade46162349395418b4f8cd8ba84
Zidras <10605951+Zidras@users.noreply.github.com>
2022-04-14 22:41:39 +0100

    Gruul+Naxx: Convert dropdown titles to spellID

--- DBM-Gruul/localization.kr.lua
+++ DBM-Gruul/localization.kr.lua
@@ -17,7 +17,7 @@ L:SetGeneralLocalization({
 
 L:SetOptionLocalization({
 	WarnGrowth	= "$spell:36300 알림 보기",
-	RangeDistance	= "|cff71d5ff|Hspell:33654|h산산조각|h|r 거리 창 범위 설정",
+	RangeDistance	= "$spell:33654 거리 창 범위 설정",
 	Smaller			= "좁은 범위 (11m)",
 	Safe			= "안전 범위 (18m)"
 })
