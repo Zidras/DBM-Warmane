7253def107bf98cc765e5863901421eb90e69dbd
Zidras <10605951+Zidras@users.noreply.github.com>
2022-06-10 21:39:29 +0100

    Revision: bump DBM.Revision
    Some important fixes were pushed during alpha stage

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -79,7 +79,7 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20220531145143"),
+	Revision = parseCurseDate("20220610045031"),
 	DisplayVersion = "9.2.20 alpha", -- the string that is shown as version
 	ReleaseRevision = releaseDate(2022, 5, 31) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
