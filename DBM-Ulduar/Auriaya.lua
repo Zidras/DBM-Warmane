2ba96526c4e0a6e97b0d9c0c565c777be7020410
Zidras <10605951+Zidras@users.noreply.github.com>
2022-06-11 15:51:32 +0100

    Auriaya: Update to 9.2 standard

--- DBM-Ulduar/Auriaya.lua
+++ DBM-Ulduar/Auriaya.lua
@@ -29,7 +29,7 @@ local timerDefender 	= mod:NewTimer(30, "timerDefender", 64455, nil, nil, 1)
 local timerFear			= mod:NewCastTimer(64386, nil, nil, nil, 4)
 local timerNextFear 	= mod:NewNextTimer(30, 64386, nil, nil, nil, 4)
 local timerNextSwarm 	= mod:NewNextTimer(36, 64396, nil, nil, nil, 1)
-local timerNextSonic 	= mod:NewNextTimer(25, 64688, nil, nil, nil, 2)
+local timerNextSonic 	= mod:NewNextTimer(25, 64688, nil, nil, nil, 2, nil, DBM_COMMON_L.DEADLY_ICON)
 local timerSonic		= mod:NewCastTimer(64688, nil, nil, nil, 2)
 
 mod.vb.catLives = 9
