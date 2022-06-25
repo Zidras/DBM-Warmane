aaf7a00add9e139828ef93e3e7dc67942f654fd5
Zidras <10605951+Zidras@users.noreply.github.com>
2022-06-25 18:50:15 +0100

    Flash: Update to 9.2 standard

--- DBM-Core/DBM-Flash.lua
+++ DBM-Core/DBM-Flash.lua
@@ -1,48 +1,49 @@
--- globals
+---------------
+--  Globals  --
+---------------
 DBM.Flash = {}
--- locals
+
+--------------
+--  Locals  --
+--------------
 local flashFrame = DBM.Flash
-local r, g, b, t, a
-local duration
-local elapsed = 0
-local totalRepeat = 0
+local frame, duration, elapsed, totalRepeat
 
 --------------------
 --  Create Frame  --
 --------------------
-local frame = CreateFrame("Frame", "DBMFlash", UIParent)
-frame:Hide()
-frame:SetBackdrop({bgFile = "Interface\\Tooltips\\UI-Tooltip-Background"})
+frame = CreateFrame("Frame", "DBMFlash", UIParent)
+frame.backdropInfo = {
+	bgFile = "Interface\\Tooltips\\UI-Tooltip-Background" -- 137056
+}
+frame:SetBackdrop(frame.backdropInfo)
 frame:SetAllPoints(UIParent)
 frame:SetFrameStrata("BACKGROUND")
+frame:Hide()
 
 ------------------------
 --  OnUpdate Handler  --
 ------------------------
-do
-	frame:SetScript("OnUpdate", function(self, e)
-		elapsed = elapsed + e
-		if elapsed >= t then
+frame:SetScript("OnUpdate", function(self, e)
+	elapsed = elapsed + e
+	if elapsed >= duration then
+		if totalRepeat == 0 then
 			self:Hide()
-			self:SetAlpha(0)
-			if totalRepeat >= 1 then--Keep repeating until totalRepeat = 0
-				flashFrame:Show(r, g, b, t, a, totalRepeat-1)
-			end
 			return
 		end
-		-- quadratic fade in/out
-		self:SetAlpha(-(elapsed / (duration / 2) - 1)^2 + 1)
-	end)
-	frame:Hide()
-end
+		elapsed = 0
+		totalRepeat = totalRepeat - 1
+		self:SetAlpha(0)
+		return
+	end
+	self:SetAlpha(-(elapsed / (duration / 2) - 1) ^ 2 + 1)
+end)
 
 function flashFrame:Show(red, green, blue, dur, alpha, repeatFlash)
-	r, g, b, t, a = red or 1, green or 0, blue or 0, dur or 0.4, alpha or 0.3
-	duration = dur
+	duration = dur or 0.4
 	elapsed = 0
 	totalRepeat = repeatFlash or 0
-	frame:SetAlpha(0)
-	frame:SetBackdropColor(r, g, b, a)
+	frame:SetBackdropColor(red or 1, green or 0, blue or 0, alpha or 0.3)
 	frame:Show()
 end
 
