b0ca81ab10ffe50f3f98194944c540ca4342b83b
Zidras <10605951+Zidras@users.noreply.github.com>
2022-07-25 22:48:41 +0100

    Vezax: timer adjustments after log review
    Warmane is experiencing some issues with CLEU:SPELL_CAST_x events, as they are not firing at random occasions. All timers have been moved to other events that seemed reliable from log review. A bug report has been opened: https://www.warmane.com/bugtracker/report/112497
    
    Saronite vapors have also been slightly reworked since this emote is range based and would cause missed emotes, ruining the add count. This forced a repeating scheduling with timer correction on synced emote for maximum reliability.

--- DBM-Ulduar/GeneralVezax.lua
+++ DBM-Ulduar/GeneralVezax.lua
@@ -1,19 +1,21 @@
 local mod	= DBM:NewMod("GeneralVezax", "DBM-Ulduar")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220716115609")
+mod:SetRevision("20220725221707")
 mod:SetCreatureID(33271)
 mod:SetUsedIcons(7, 8)
 
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 62661 62662",
+--	"SPELL_CAST_START 62661 62662",
 	"SPELL_INTERRUPT 62661",
-	"SPELL_AURA_APPLIED 62662",
+	"SPELL_AURA_APPLIED 62662 63276",
 	"SPELL_AURA_REMOVED 62662",
-	"SPELL_CAST_SUCCESS 62660 63276 63364",
-	"CHAT_MSG_RAID_BOSS_EMOTE"
+--	"SPELL_CAST_SUCCESS 62660 63276 63364",
+	"CHAT_MSG_RAID_BOSS_EMOTE",
+	"UNIT_SPELLCAST_START boss1",
+	"UNIT_SPELLCAST_SUCCEEDED boss1"
 )
 
 local warnShadowCrash				= mod:NewTargetAnnounce(62660, 4)
@@ -32,11 +34,11 @@ local specWarnSearingFlames			= mod:NewSpecialWarningInterruptCount(62661, "HasI
 local timerEnrage					= mod:NewBerserkTimer(600)
 local timerSearingFlamesCast		= mod:NewCastTimer(2, 62661, nil, nil, nil, 5, nil, DBM_COMMON_L.INTERRUPT_ICON)
 local timerSurgeofDarkness			= mod:NewBuffActiveTimer(10, 62662, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON)
-local timerNextSurgeofDarkness		= mod:NewCDTimer(51.9, 62662, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON) -- REVIEW! 10s variance [50-60; 50-70 from TC]? (25 man NM log review 2022/07/10) - 58.0, 51.9
-local timerSaroniteVapors			= mod:NewNextCountTimer(30, 63322, nil, nil, nil, 5, nil, DBM_COMMON_L.HEALER_ICON) -- REVIEW! 37s variance [30-67] or emote not fired? (25 man NM log review 2022/07/10) - 67.5, 34.2
+local timerNextSurgeofDarkness		= mod:NewCDTimer(51.9, 62662, nil, "Tank", nil, 5, nil, DBM_COMMON_L.TANK_ICON) -- REVIEW! 10s variance [50-60; 50-70 from TC]? (25 man NM log 2022/07/10 || S3 HM log 2022/07/21) - 58.0, 51.9 || 55.7, 60.1
+local timerSaroniteVapors			= mod:NewNextCountTimer(30, 63322, nil, nil, nil, 5, nil, DBM_COMMON_L.HEALER_ICON) -- Emote not fired if out of range (confirmed as of 2022/07/25). Has variance, but apply 30s regardless
 local timerShadowCrashCD			= mod:NewCDTimer(8.1, 62660, nil, "Ranged", nil, 3) -- 4s variance [8-12] (25 man NM log review 2022/07/10) - 8.8, 10.5, 8.6, 10.6, 10.3, 8.1, 11.8, 9.7, 10.6, 9.8, 14.2, 9.3, 9.1, 8.7, 8.4, 11.5, 8.9
 local timerMarkoftheFaceless		= mod:NewTargetTimer(10, 63276, nil, false, 2, 3, nil, DBM_COMMON_L.IMPORTANT_ICON)
-local timerMarkoftheFacelessCD		= mod:NewCDTimer(35.9, 63276, nil, nil, nil, 3, nil, DBM_COMMON_L.IMPORTANT_ICON) -- REVIEW! 15s variance [35-45]? (25 man NM log review 2022/07/10) - 35.9, 41.1, 36.6
+local timerMarkoftheFacelessCD		= mod:NewCDTimer(35.9, 63276, nil, nil, nil, 3, nil, DBM_COMMON_L.IMPORTANT_ICON) -- 15s variance [35-45] (25 man NM log 2022/07/10 || S3 HM log 2022/07/21) - 35.9, 41.1, 36.6 || 36.3, 37.6, 37.9, 43.9, 43.8, 40.1
 
 mod:AddSetIconOption("SetIconOnShadowCrash", 62660, true, false, {8})
 mod:AddSetIconOption("SetIconOnLifeLeach", 63276, true, false, {7})
@@ -50,6 +52,15 @@ local timerHardmode				= mod:NewTimer(212, "hardmodeSpawn", nil, nil, nil, 1) --
 
 mod.vb.interruptCount = 0
 mod.vb.vaporsCount = 0
+
+local function saroniteVaporsSpawned(self)
+	self.vb.vaporsCount = self.vb.vaporsCount + 1
+	warnSaroniteVapor:Show(self.vb.vaporsCount)
+	if self.vb.vaporsCount < 6 then
+		timerSaroniteVapors:Start(nil, self.vb.vaporsCount + 1)
+		self:Schedule(30, saroniteVaporsSpawned, self) -- only first 6 spawns are relevant for Hard Mode
+	end
+end
 
 function mod:ShadowCrashTarget(targetname, uId)
 	if not targetname then return end
@@ -79,15 +90,17 @@ end
 function mod:OnCombatStart(delay)
 	self.vb.interruptCount = 0
 	self.vb.vaporsCount = 0
-	timerShadowCrashCD:Start(8.0-delay) -- REVIEW! variance 8-12s? 25 man NM log review 2022/07/10 - 8.0
-	timerMarkoftheFacelessCD:Start(37.3-delay) -- REVIEW! variance 35-45s? 25 man NM log review 2022/07/10 - 37.3
+	timerShadowCrashCD:Start(8.0-delay) -- REVIEW! variance [8-12s?] (25 man NM log 2022/07/10 || S3 HM log 2022/07/21) - 8.0 || 9.6
+	timerMarkoftheFacelessCD:Start(35.1-delay) -- REVIEW! variance 35-45s? (25 man NM log 2022/07/10 || S3 HM log 2022/07/21) - 37.3 || 35.1
 	timerSaroniteVapors:Start(30.0-delay, 1) -- Log reviewed (25 man NM log review 2022/07/10)
+	self:Schedule(30-delay, saroniteVaporsSpawned, self)
 	timerEnrage:Start(-delay)
 	timerHardmode:Start(-delay)
-	timerNextSurgeofDarkness:Start(60.0-delay) -- REVIEW! 10s variance [50-60]? (25 man NM log review 2022/07/10) - 60.0
+	timerNextSurgeofDarkness:Start(60.0-delay) -- REVIEW! 10s variance [50-60]? (25 man NM log 2022/07/10 || S3 HM log 2022/07/21) - 60.0 || 60.0
 end
 
-function mod:SPELL_CAST_START(args)
+-- Confirmed as of 2022/07/25: CLEU SPELL_CAST_x is randomly failing on Warmane and not firing - https://www.warmane.com/bugtracker/report/112497. Resort to AURA/UNIT_SPELLCAST_x events for now.
+--[[function mod:SPELL_CAST_START(args)
 	local spellId = args.spellId
 	if spellId == 62661 then	-- Searing Flames
 		self.vb.interruptCount = self.vb.interruptCount + 1
@@ -105,7 +118,7 @@ function mod:SPELL_CAST_START(args)
 		end
 		timerNextSurgeofDarkness:Start()
 	end
-end
+end]]
 
 function mod:SPELL_INTERRUPT(args)
 	if args.spellId == 62661 then
@@ -114,8 +127,25 @@ function mod:SPELL_INTERRUPT(args)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 62662 then	-- Surge of Darkness
+	local spellId = args.spellId
+	if spellId == 62662 then	-- Surge of Darkness
 		timerSurgeofDarkness:Start()
+	elseif spellId == 63276 then	-- Mark of the Faceless
+		if self.Options.SetIconOnLifeLeach then
+			self:SetIcon(args.destName, 7, 10)
+		end
+		timerMarkoftheFaceless:Start(args.destName)
+		timerMarkoftheFacelessCD:Start()
+		if args:IsPlayer() then
+			specWarnMarkoftheFacelessYou:Show()
+			specWarnMarkoftheFacelessYou:Play("runout")
+			yellMarkoftheFaceless:Yell()
+		elseif self:CheckNearby(11, args.destName) then
+			specWarnMarkoftheFacelessNear:Show(args.destName)
+			specWarnMarkoftheFacelessNear:Play("runaway")
+		else
+			warnLeechLife:Show(args.destName)
+		end
 	end
 end
 
@@ -125,7 +155,8 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
-function mod:SPELL_CAST_SUCCESS(args)
+-- Confirmed as of 2022/07/25: CLEU SPELL_CAST_x is randomly failing on Warmane and not firing - https://www.warmane.com/bugtracker/report/112497. Resort to AURA/UNIT_SPELLCAST_x events for now.
+--[[function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
 	if spellId == 62660 then		-- Shadow Crash
 		self:BossTargetScanner(33271, "ShadowCrashTarget", 0.05, 20)
@@ -150,20 +181,50 @@ function mod:SPELL_CAST_SUCCESS(args)
 		specWarnAnimus:Show()
 		specWarnAnimus:Play("bigmob")
 	end
-end
+end]]
 
+-- Range restriced, not reliable enough to sync CHAT_MSG_RAID_BOSS_EMOTE, so a repeating schedule was implemented instead and emote is only used for timer correction.
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(emote)
 	if emote == L.EmoteSaroniteVapors or emote:find(L.EmoteSaroniteVapors) then
-		self:SendSync("SaroniteVaporsSpawned") -- REVIEW! Range restriced, so attempt to work around it by syncing. If still not reliable enough, implement a repeater scheduling instead.
+		self:SendSync("SaroniteVaporsSpawned")
+	end
+end
+
+function mod:UNIT_SPELLCAST_START(_, spellName)
+	if spellName == GetSpellInfo(62661) then	-- Searing Flames
+		self.vb.interruptCount = self.vb.interruptCount + 1
+		if self.vb.interruptCount == 4 then
+			self.vb.interruptCount = 1
+		end
+		local kickCount = self.vb.interruptCount
+		specWarnSearingFlames:Show(L.name, kickCount)
+		specWarnSearingFlames:Play("kick"..kickCount.."r")
+		timerSearingFlamesCast:Start()
+	elseif spellName == GetSpellInfo(62662) then
+		if self:IsTanking("player", "boss1", nil, true) then--Player is current target
+			specWarnSurgeDarkness:Show()
+			specWarnSurgeDarkness:Play("defensive")
+		end
+		timerNextSurgeofDarkness:Start()
+	end
+end
+
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName)
+	if spellName == GetSpellInfo(62660) then		-- Shadow Crash
+		self:BossTargetScanner(33271, "ShadowCrashTarget", 0.05, 20)
+		timerShadowCrashCD:Start()
+	elseif spellName == GetSpellInfo(63364) then
+		self:Unschedule(saroniteVaporsSpawned)
+		timerSaroniteVapors:Stop()
+		specWarnAnimus:Show()
+		specWarnAnimus:Play("bigmob")
 	end
 end
 
 function mod:OnSync(msg)
 	if msg == "SaroniteVaporsSpawned" and self:AntiSpam(3, 1) then
-		self.vb.vaporsCount = self.vb.vaporsCount + 1
-		warnSaroniteVapor:Show(self.vb.vaporsCount)
-		if self.vb.vaporsCount < 6 then
-			timerSaroniteVapors:Start(nil, self.vb.vaporsCount+1)
-		end
+		self:Unschedule(saroniteVaporsSpawned)
+		self:Schedule(30, saroniteVaporsSpawned, self)
+		timerSaroniteVapors:Restart(30, self.vb.vaporsCount + 1)
 	end
 end
