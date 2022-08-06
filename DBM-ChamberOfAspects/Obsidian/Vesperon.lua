6e4f3bbd6bf884c466caa9f546c137f0012475ec
Zidras <10605951+Zidras@users.noreply.github.com>
2022-08-06 12:17:00 +0100

    OS: Prevent Shadron, Tenebron, and Vesperon from engaging if Sartharian is already engaged
    Will also prevent multi-pull of the drakes, but who is doing that anyway...

--- DBM-ChamberOfAspects/Obsidian/Vesperon.lua
+++ DBM-ChamberOfAspects/Obsidian/Vesperon.lua
@@ -3,8 +3,11 @@ local L		= mod:GetLocalizedStrings()
 
 mod.statTypes = "normal,normal25"
 
-mod:SetRevision("20220518110528")
+mod:SetRevision("20220806121300")
 mod:SetCreatureID(30449)
+mod:SetHotfixNoticeRev(20220805000000)
+mod:SetMinSyncRevision(20220805000000)
+mod:DisableMultiBossPulls()
 
 mod:RegisterCombat("combat")
 
@@ -12,7 +15,7 @@ mod:RegisterEventsInCombat(
 	"SPELL_CAST_SUCCESS 57579 59127"
 )
 
-local warnShadowFissure		= mod:NewSpellAnnounce(59127, nil, nil, nil, nil, nil, 2)
+local warnShadowFissure		= mod:NewSpellAnnounce(59127, 4, nil, nil, nil, nil, 2)
 local timerShadowFissure	= mod:NewCastTimer(5, 59128, nil, nil, nil, 3)--Cast timer until Void Blast. it's what happens when shadow fissure explodes.
 
 mod:GroupSpells(59127, 59128)--Shadow fissure with void blast
