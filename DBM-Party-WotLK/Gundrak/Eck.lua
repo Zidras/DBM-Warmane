9f8f27d8437b2090558d1eb96c6be7edad1f0a59
Zidras <10605951+Zidras@users.noreply.github.com>
2022-08-06 17:32:09 +0100

    Gundrak: add Mythic statTypes

--- DBM-Party-WotLK/Gundrak/Eck.lua
+++ DBM-Party-WotLK/Gundrak/Eck.lua
@@ -1,6 +1,8 @@
 local mod	= DBM:NewMod("Eck", "DBM-Party-WotLK", 5)
 local L		= mod:GetLocalizedStrings()
 
+mod.statTypes = "normal,heroic,mythic"
+
 mod:SetRevision("20220518110528")
 mod:SetCreatureID(29932)
 
