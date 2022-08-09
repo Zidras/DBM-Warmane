3bee5150c66e384bbc3a7d92567a4d08c2823704
Zidras <10605951+Zidras@users.noreply.github.com>
2022-08-09 18:28:42 +0100

    HallsOfStone-Brann: adjust event timer

--- DBM-Party-WotLK/HallsOfStone/BrannBronzebeard.lua
+++ DBM-Party-WotLK/HallsOfStone/BrannBronzebeard.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("BrannBronzebeard", "DBM-Party-WotLK", 7)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220518110528")
+mod:SetRevision("20220809182722")
 mod:SetCreatureID(28070)
 mod:SetMinSyncRevision(2861)
 
@@ -16,7 +16,7 @@ mod:RegisterEventsInCombat(
 
 local warningPhase	= mod:NewAnnounce("WarningPhase", 2, "Interface\\Icons\\Spell_Nature_WispSplode")
 
-local timerEvent	= mod:NewTimer(302, "timerEvent", "Interface\\Icons\\Spell_Holy_BorrowedTime", nil, nil, 6)
+local timerEvent	= mod:NewTimer(247, "timerEvent", "Interface\\Icons\\Spell_Holy_BorrowedTime", nil, nil, 6)
 
 function mod:OnCombatStart(delay)
 	timerEvent:Start(-delay)
