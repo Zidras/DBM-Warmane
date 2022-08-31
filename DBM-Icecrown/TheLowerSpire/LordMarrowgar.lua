4086d4a113029db32f51ff9f058255f730d9b76b
Zidras <10605951+Zidras@users.noreply.github.com>
2022-08-31 23:13:56 +0100

    Marrowgar: fix Bone Spike Graveyard timer
    - Added a "keep" arg to account for timer variance (from the few logs I have it ranges from 15 to 20). Need more data to validate.
    - Included missing spellIDs related to this spell that boss casts during bone storm (even though there's equivalent IDs for N10 and N25, couldn't find a reason to add them since afaik these don't happen during bone storm),

--- DBM-Icecrown/TheLowerSpire/LordMarrowgar.lua
+++ DBM-Icecrown/TheLowerSpire/LordMarrowgar.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("LordMarrowgar", "DBM-Icecrown", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220624005857")
+mod:SetRevision("20220831230337")
 mod:SetCreatureID(36612)
 mod:SetUsedIcons(1, 2, 3, 4, 5, 6, 7, 8)
 
@@ -10,7 +10,7 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 69076",
 	"SPELL_AURA_REMOVED 69065 69076",
-	"SPELL_CAST_START 69057 70826 72088 72089 69076",
+	"SPELL_CAST_START 69057 70826 72088 72089 73144 73145 69076",
 	"SPELL_PERIODIC_DAMAGE",
 	"SPELL_PERIODIC_MISSED",
 	"SPELL_SUMMON 69062 72669 72670"
@@ -25,7 +25,7 @@ local warnImpale			= mod:NewTargetNoFilterAnnounce(72669, 3)
 local specWarnColdflame		= mod:NewSpecialWarningGTFO(69146, nil, nil, nil, 1, 8)
 local specWarnWhirlwind		= mod:NewSpecialWarningRun(69076, nil, nil, nil, 4, 2)
 
-local timerBoneSpike		= mod:NewCDTimer(18, 69057, nil, nil, nil, 1, nil, DBM_COMMON_L.DAMAGE_ICON)
+local timerBoneSpike		= mod:NewCDTimer(15, 69057, nil, nil, nil, 1, nil, DBM_COMMON_L.DAMAGE_ICON, true) -- REVIEW! 5s variance [15-20]? Added "keep" arg
 local timerWhirlwindCD		= mod:NewCDTimer(30, 69076, nil, nil, nil, 2, nil, DBM_COMMON_L.MYTHIC_ICON)
 local timerWhirlwind		= mod:NewBuffActiveTimer(20, 69076, nil, nil, nil, 6)
 local timerBoned			= mod:NewAchievementTimer(8, 4610)
@@ -81,7 +81,7 @@ function mod:SPELL_AURA_REMOVED(args)
 end
 
 function mod:SPELL_CAST_START(args)
-	if args:IsSpellID(69057, 70826, 72088, 72089) then	-- Bone Spike Graveyard
+	if args:IsSpellID(69057, 70826, 72088, 72089) or args:IsSpellID(73144, 73145) then	-- Bone Spike Graveyard (no bone storm) | (during bone storm HC). REVIEW!
 		warnBoneSpike:Show()
 		timerBoneSpike:Start()
 		timerBoneSpikeUp:Start()
