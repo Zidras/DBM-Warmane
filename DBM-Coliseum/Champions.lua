586ee2ff9bee42df8bc2ccdd1b9a07f22cd59761
Zidras <10605951+Zidras@users.noreply.github.com>
2022-09-25 11:50:32 +0100

    ToC-Champions: update timers

--- DBM-Coliseum/Champions.lua
+++ DBM-Coliseum/Champions.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Champions", "DBM-Coliseum")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220907232753")
+mod:SetRevision("20220925115014")
 mod:SetCreatureID(34458, 34451, 34459, 34448, 34449, 34445, 34456, 34447, 34441, 34454, 34444, 34455, 34450, 34453, 34461, 34460, 34469, 34467, 34468, 34471, 34465, 34466, 34473, 34472, 34470, 34463, 34474, 34475)
 mod:SetMinSyncRevision(20220907000000)
 
@@ -127,10 +127,10 @@ local timerShadowstepCD		= mod:NewCDTimer(30, 66178, nil, nil, nil, 3) -- (25H L
 local timerBlindCD			= mod:NewCDTimer(120, 65960)
 local timerDeathgripCD		= mod:NewCDTimer(21.5, 66017, nil, nil, nil, 3) -- REVIEW! High variance (25H Lordaeron 2022/09/03) - pull:22.9, 31.5, 45.1, 34.9, 24.2, 21.9, 31.1, 61.4, 43.9, 21.5
 local timerBladestormCD		= mod:NewCDTimer(90.1, 65947, nil, nil, nil, 2) -- (25H Lordaeron 2022/09/03) - pull:49.0, 90.1, 90.1
-local timerFrostTrapCD		= mod:NewCDTimer(31.8, 65880) -- REVIEW! variance? (25H Lordaeron 2022/09/03) - pull:46.2, 31.8
+local timerFrostTrapCD		= mod:NewCDTimer(31.6, 65880) -- REVIEW! variance? (25H Lordaeron 2022/09/03 || 25H Lordaeron 2022/09/23-npc:34467) - pull:46.2, 31.8 || pull:22.7, 34.4, 31.6
 local timerDisengageCD		= mod:NewCDTimer(30, 65869) -- REVIEW! variance? (25H Lordaeron 2022/09/03) - pull:32.6, 40.4
 local timerPsychicScreamCD	= mod:NewCDTimer(30, 65543) -- variance (25H Lordaeron 2022/09/03) - pull:49.1, 30.0, 31.7, 30.1, 33.5, 31.3, 30.6, 44.0, 37.2, 31.0
-local timerBlinkCD			= mod:NewCDTimer(17.6, 65793) -- REVIEW! High variance (25H Lordaeron 2022/09/03) - pull:21.1, 17.6, 135.2, 20.2, 44.0, 34.7, 27.3, 18.1, 20.6, 19.2
+local timerBlinkCD			= mod:NewCDTimer(15.1, 65793) -- REVIEW! High variance! diff script per npc? (25H Lordaeron 2022/09/03-npc:34449 || 25H Lordaeron 2022/09/23-npc:34468) - pull:21.1, 17.6, 135.2, 20.2, 44.0, 34.7, 27.3, 18.1, 20.6, 19.2 || pull:19.3, 15.1, 28.5, 15.1, 19.3, 21.2, 92.1, 19.3
 local timerHoJCD			= mod:NewCDTimer(40, 66613) -- REVIEW! variance? (25H Lordaeron 2022/09/03) - pull:178.8
 local timerRepentanceCD		= mod:NewCDTimer(60, 66008)
 local timerHoPCD			= mod:NewCDTimer(300, 66009) -- REVIEW! variance? (25H Lordaeron 2022/09/03) - pull:32.5
