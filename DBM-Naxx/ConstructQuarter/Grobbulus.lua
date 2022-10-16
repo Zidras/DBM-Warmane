91c71ed1990a280519b65e9a743a7e9fa3d373a4
Zidras <10605951+Zidras@users.noreply.github.com>
2022-10-16 19:13:43 +0100

    Naxx-Grobbulus: update Slime Spray timer

--- DBM-Naxx/ConstructQuarter/Grobbulus.lua
+++ DBM-Naxx/ConstructQuarter/Grobbulus.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Grobbulus", "DBM-Naxx", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220116041927")
+mod:SetRevision("20221016191326")
 mod:SetCreatureID(15931)
 mod:SetUsedIcons(1, 2, 3, 4)
 
@@ -23,7 +23,7 @@ local yellInjection			= mod:NewYellMe(28169, nil, false)
 
 local timerInjection		= mod:NewTargetTimer(10, 28169, nil, nil, nil, 3)
 local timerCloud			= mod:NewNextTimer(15, 28240, nil, nil, nil, 5, nil, DBM_COMMON_L.TANK_ICON)
-local timerSlimeSpray		= mod:NewNextTimer(32, 54364, nil, nil, nil, 2)
+local timerSlimeSprayCD		= mod:NewCDTimer(32, 54364, nil, nil, nil, 2) -- Transcriptor snippet below
 local enrageTimer			= mod:NewBerserkTimer(720)
 
 mod:AddSetIconOption("SetIconOnInjectionTarget", 28169, false, false, {1, 2, 3, 4})
@@ -53,7 +53,7 @@ function mod:OnCombatStart(delay)
 	table.wipe(mutateIcons)
 	enrageTimer:Start(-delay)
 	warnSlimeSpraySoon:Schedule(27)
-	timerSlimeSpray:Start()
+	timerSlimeSprayCD:Start(31) -- REVIEW! variance? (25man Lordaeron 2022/10/16) - 31.0
 end
 
 function mod:OnCombatEnd()
@@ -94,12 +94,13 @@ function mod:SPELL_CAST_SUCCESS(args)
 	elseif args:IsSpellID(28157, 54364) then
 		warnSlimeSprayNow:Show()
 		self.vb.slimeSprays = self.vb.slimeSprays + 1
+		 -- REVIEW! variance? (25man Lordaeron 2022/10/16) - pull:31.0, 27.7, 61.1, 25.5
 		if self.vb.slimeSprays % 2 == 0 then -- every 2/4/6... spray short cd
-			warnSlimeSpraySoon:Schedule(26)
-			timerSlimeSpray:Start(31)
+			warnSlimeSpraySoon:Schedule(20.5)
+			timerSlimeSprayCD:Start(25.5)
 		else -- every 3/5/7... spray long cd
 			warnSlimeSpraySoon:Schedule(54)
-			timerSlimeSpray:Start(59)
+			timerSlimeSprayCD:Start(59)
 		end
 	end
 end
