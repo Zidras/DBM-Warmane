fbe307181209cbbe7affa7987a01b07bbccf1ae5
Zidras <10605951+Zidras@users.noreply.github.com>
2023-01-29 11:44:33 +0000

    Update TeronGorefiend.lua

--- DBM-BlackTemple/TeronGorefiend.lua
+++ DBM-BlackTemple/TeronGorefiend.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("TeronGorefiend", "DBM-BlackTemple")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220518110528")
+mod:SetRevision("20230129114417")
 mod:SetCreatureID(22871)
 
 mod:SetModelID(21254)
