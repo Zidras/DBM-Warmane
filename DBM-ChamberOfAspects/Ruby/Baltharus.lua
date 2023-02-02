c77fe3a70f5b517da57c59baccfe181a86dcd442
Zidras <10605951+Zidras@users.noreply.github.com>
2023-02-02 21:28:47 +0000

    RS-Baltharus: add Blade Tempest timer

--- DBM-ChamberOfAspects/Ruby/Baltharus.lua
+++ DBM-ChamberOfAspects/Ruby/Baltharus.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Baltharus", "DBM-ChamberOfAspects", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220518110528")
+mod:SetRevision("20230203003159")
 mod:SetCreatureID(39751)
 mod:SetUsedIcons(1, 2, 3, 4, 5, 6, 7, 8)
 
@@ -10,6 +10,7 @@ mod:RegisterCombat("combat")
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_START 74509",
 	"SPELL_AURA_APPLIED 75125 74505",
+	"INSTANCE_ENCOUNTER_ENGAGE_UNIT",
 	"UNIT_HEALTH boss1"
 )
 
@@ -23,6 +24,7 @@ local specWarnRepellingWave	= mod:NewSpecialWarningSpell(74509, nil, nil, nil, 2
 local timerWhirlwind		= mod:NewBuffActiveTimer(4, 75125, nil, "Tank|Healer", nil, 3)
 local timerRepellingWave	= mod:NewCastTimer(4, 74509, nil, nil, nil, 2)--1 second cast + 3 second stun
 local timerBrand			= mod:NewBuffActiveTimer(10, 74505, nil, nil, nil, 5)
+local timerBladeTempest		= mod:NewCDCountTimer("d24", 75125, nil, nil, 5) -- String timer starting with "d" means "allowDouble". REVIEW! ~1s variance? (boss1 timers: 25N Lordaeron 2022-09-19 || 25H Lordaeron 2022-09-23) - "Blade Tempest-75125-npc:39751-2 = pull:15.0, 25.2, 24.0, 25.2" || "Blade Tempest-75125-npc:39751-2 = pull:15.0, 25.2, 25.2"
 
 mod:AddRangeFrameOption(12, 74505)
 mod:AddSetIconOption("SetIconOnBrand", 74505, false, false, {1, 2, 3, 4, 5, 6, 7, 8})
@@ -32,6 +34,7 @@ mod.vb.warnedSplit2	= false
 mod.vb.warnedSplit3	= false
 local brandTargets = {}
 mod.vb.brandIcon	= 8
+mod.vb.allClonesSpawned = false
 
 local function showBrandWarning(self)
 	warningWarnBrand:Show(table.concat(brandTargets, "<, >"))
@@ -45,6 +48,8 @@ function mod:OnCombatStart()
 	self.vb.warnedSplit3 = false
 	table.wipe(brandTargets)
 	self.vb.brandIcon = 8
+	self.vb.allClonesSpawned = false
+	timerBladeTempest:Start(15, 1, UnitGUID("boss1")) -- REVIEW! (25N Lordaeron 2022-09-19 || 25H Lordaeron 2022-09-23) - 15.0 || 15.0
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Show(12)
 	end
@@ -67,8 +72,10 @@ end
 function mod:SPELL_AURA_APPLIED(args)
 	local spellId = args.spellId
 	if spellId == 75125 then
+		local bossN = tonumber(string.match(DBM:GetUnitIdFromGUID(args.destGUID, true) or "0", "%d+")) or 0
 		warnWhirlwind:Show()
 		timerWhirlwind:Show()
+		timerBladeTempest:Start(nil, bossN, args.destGUID)
 	elseif spellId == 74505 and self:IsInCombat() then--Only do this when boss is actually engaged, otherwise it doesn't really matter and just spams.
 		brandTargets[#brandTargets + 1] = args.destName
 		if args:IsPlayer() then
@@ -84,6 +91,16 @@ function mod:SPELL_AURA_APPLIED(args)
 		end
 		self:Unschedule(showBrandWarning)
 		self:Schedule(0.5, showBrandWarning, self)
+	end
+end
+
+function mod:INSTANCE_ENCOUNTER_ENGAGE_UNIT()
+	if self.vb.allClonesSpawned == true then return end
+	if UnitExists("boss3") then
+		self.vb.allClonesSpawned = true
+		timerBladeTempest:Start(20, 3, UnitGUID("boss3")) -- REVIEW! 5s variance? Based on clone spawn? (25N Lordaeron 2022-09-19 || 25H Lordaeron 2022-09-23) - 20.7 || 20.2
+	elseif UnitExists("boss2") then
+		timerBladeTempest:Start(20, 2, UnitGUID("boss2")) -- REVIEW! 5s variance? Based on clone spawn? (25N Lordaeron 2022-09-19 || 25H Lordaeron 2022-09-23) - 24.3 || 19.9
 	end
 end
 
