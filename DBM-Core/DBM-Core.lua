6837c58586e51f80d81dcf804454b56f3c21615c
Zidras <10605951+Zidras@users.noreply.github.com>
2023-03-11 19:47:15 +0000

    Version: prepare release 9.2.26

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,9 +82,9 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20230212114559"),
-	DisplayVersion = "9.2.26 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2022, 11, 1) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	Revision = parseCurseDate("20230311194447"),
+	DisplayVersion = "9.2.26", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2023, 3, 11) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
 local fakeBWVersion = 7558
