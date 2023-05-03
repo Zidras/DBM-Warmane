86f9d0e6cae6cbb62fb4611c506342b9293b0138
Zidras <10605951+Zidras@users.noreply.github.com>
2023-05-03 22:50:23 +0100

    Version: new 10.0.27_alpha cycle

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,8 +82,8 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20230503224812"),
-	DisplayVersion = "10.0.26", -- the string that is shown as version
+	Revision = parseCurseDate("20230503224954"),
+	DisplayVersion = "10.0.27 alpha", -- the string that is shown as version
 	ReleaseRevision = releaseDate(2023, 5, 3, 22) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
