7cced1976423e173690d07546ef8e26cd1543a91
Zidras <10605951+Zidras@users.noreply.github.com>
2023-05-03 22:07:56 +0100

    Version: prepare release 10.0.25
    Purposefully left out comms rewrite with protocol handshake, since I must ensure backwards compatibility with comms sent to and from old DBMs.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,9 +82,9 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20230503220454"),
-	DisplayVersion = "10.0.25 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2023, 4, 21) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	Revision = parseCurseDate("20230503220612"),
+	DisplayVersion = "10.0.25", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2023, 5, 3) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
 local fakeBWVersion = 7558
