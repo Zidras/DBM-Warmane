45bccdf0a7215fd6b32e153ef4964c5d12a073eb
Zidras <10605951+Zidras@users.noreply.github.com>
2023-05-07 15:45:36 +0100

    Version: prepare release 10.0.29
    Force disable to allow forcing outside of tests and major patches was NOT backported, since I can see potential for abuse.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,9 +82,9 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20230507154222"),
-	DisplayVersion = "10.0.29 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2023, 5, 3, 23) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	Revision = parseCurseDate("20230507154433"),
+	DisplayVersion = "10.0.29", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2023, 5, 7) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
 local fakeBWVersion = 7558
