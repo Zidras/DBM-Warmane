02cfbf211e7fc72a1daf9604981f1d8a9dd25349
Zidras <10605951+Zidras@users.noreply.github.com>
2023-05-11 23:21:59 +0100

    Voice: add few missing voices and sync some others

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,7 +82,7 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20230511230628"),
+	Revision = parseCurseDate("20230511231638"),
 	DisplayVersion = "10.1.6 alpha", -- the string that is shown as version
 	ReleaseRevision = releaseDate(2023, 5, 11) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
