93ce5d639e72faec2f09b407f55580ac6964eabb
Zidras <10605951+Zidras@users.noreply.github.com>
2023-08-24 10:49:15 +0100

    UBRS-PyroguardEmberseer: prevent pull timer from increasing with multiple altar clicks

--- DBM-Party-Classic/UpperBlackrockSpire/PyroguardEmberseer.lua
+++ DBM-Party-Classic/UpperBlackrockSpire/PyroguardEmberseer.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("PyroguardEmberseer", "DBM-Party-Classic", 4)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220518110528")
+mod:SetRevision("20230824104734")
 mod:SetCreatureID(9816)
 
 mod:RegisterCombat("combat")
@@ -13,7 +13,7 @@ mod:RegisterEvents(
 local timerCombatStart	= mod:NewCombatTimer(64)
 
 function mod:CHAT_MSG_MONSTER_EMOTE(msg)
-	if msg == L.Pull or msg:find(L.Pull) then
+	if msg == L.Pull or msg:find(L.Pull) and not timerCombatStart:IsStarted() then
 		timerCombatStart:Start()
 	end
 end
