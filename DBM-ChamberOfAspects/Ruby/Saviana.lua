524aaca404d321b8a444cf965bec6f3e246f3425
Zidras <10605951+Zidras@users.noreply.github.com>
2023-08-26 16:08:24 +0100

    RS-Saviana: attempt more fidelity on Breath timer
    Pause/Resume on phasing seems like a good middle ground. It's not really accurate, since I suspect the 10s variance window is preserved on phasing

--- DBM-ChamberOfAspects/Ruby/Saviana.lua
+++ DBM-ChamberOfAspects/Ruby/Saviana.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Saviana", "DBM-ChamberOfAspects", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20230824234130")
+mod:SetRevision("20230826155950")
 mod:SetCreatureID(39747)
 mod:SetUsedIcons(8, 7, 6, 5, 4)
 
@@ -23,8 +23,10 @@ local specWarnTranq			= mod:NewSpecialWarningDispel(78722, "RemoveEnrage", nil, 
 local timerBeacon			= mod:NewBuffActiveTimer(5, 74453, nil, nil, nil, 3)
 local timerConflag			= mod:NewBuffActiveTimer(5, 74456, nil, nil, nil, 3)
 local timerConflagCD		= mod:NewCDTimer(63.8, 74452, nil, nil, nil, 3) -- REVIEW! Using UNIT_SPELLCAST_SUCCEEDED since it only fires once. 1s variance? if it's this low, not worth enabling "Keep" (25N Lordaeron 2022/09/19 || 25H Lordaeron 2022/09/23) -- 63.8 || 64.3
-local timerBreath			= mod:NewCDTimer(19.3, 74403, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON, true) -- REVIEW! ~17s variance! Added "Keep" arg (25N Lordaeron 2022/09/19 || 25H Lordaeron 2022/09/23 || 25N Lordaeron [2023-06-27]@[19:12:05]) -- 38.8, 29.5, 34.2 || 38.4, 25.7 || 38.1, 22.1
+local timerBreath			= mod:NewCDTimer(19.3, 74403, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON, true) -- REVIEW! ~10s variance [19.3-29.5] Added "Keep" arg (25N Lordaeron 2022/09/19 || 25H Lordaeron 2022/09/23 || 25N Lordaeron [2023-06-27]@[19:12:05]) -- 38.8, 29.5, 34.2 || 38.4, 25.7 || 38.1, 22.1
 local timerEnrage			= mod:NewBuffActiveTimer(10, 78722, nil, "RemoveEnrage|Tank|Healer", nil, 5, nil, DBM_COMMON_L.ENRAGE_ICON..DBM_COMMON_L.TANK_ICON)
+local timerFlight			= mod:NewNextTimer(50, 34873, nil, nil, nil, 6, 43810)
+local timerLanding			= mod:NewNextTimer(8, 30202, nil, nil, nil, 6, 43810)
 
 mod:AddRangeFrameOption(10, 74456)
 mod:AddSetIconOption("beaconIcon", 74453, true, false, {8, 7, 6, 5, 4})
@@ -46,21 +48,26 @@ local function savianaPhaseCatcher(self)
 	)
 end
 
-local function savianaLanding(self)
-	self:SetStage(1)
-	self:UnregisterShortTermEvents()
-end
-
 local function savianaAirphase(self)
 	self:SetStage(1.5)
+	timerBreath:Pause()
 	self:UnregisterShortTermEvents()
 end
 
+local function savianaLanding(self)
+	self:SetStage(1)
+	timerFlight:Start()
+	timerBreath:Resume()
+	self:Schedule(49.5, savianaPhaseCatcher, self)
+	self:Schedule(50, savianaAirphase, self)
+	self:UnregisterShortTermEvents()
+end
 
 function mod:OnCombatStart(delay)
 	self:SetStage(1)
 	timerConflagCD:Start(30.1-delay) -- REVIEW! variance? (25N Lordaeron 2022/09/19 || 25H Lordaeron 2022/09/23) -- 30.1 || 30.2
 	timerBreath:Start(14-delay) -- (25N Lordaeron 2022/09/19 || 25H Lordaeron 2022/09/23 || 25N Lordaeron [2023-06-27]@[19:12:05]) - 14.0 || 14.0 || 14.0
+	timerFlight:Start(25-delay)
 	table.wipe(beaconTargets)
 	self.vb.beaconIcon = 8
 	if self.Options.RangeFrame then
@@ -116,6 +123,7 @@ end
 function mod: UNIT_SPELLCAST_SUCCEEDED(_, spellName) -- UNIT_SPELLCAST_START/CLEU fires and stops right after, and only gets SUCCEEDED one second after, one time only, which is better to optimize some calls
 	if spellName == GetSpellInfo(74454) then -- Conflagration
 		timerConflagCD:Start()
+		timerLanding:Start()
 		self:Schedule(7, savianaPhaseCatcher, self)
 		self:Schedule(7.89, savianaLanding, self) -- Lowest 7.88
 	end
