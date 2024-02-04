b03064dfc1373ca175210b42e4fa97e524fd1764
Zidras <10605951+Zidras@users.noreply.github.com>
2024-02-04 19:22:06 +0000

    BWL-Broodlord: add KnockAway timer and fix spellID
    Warmane is using wrong ID for this spell:  https://www.warmane.com/bugtracker/report/120897

--- DBM-BWL/Broodlord.lua
+++ DBM-BWL/Broodlord.lua
@@ -1,14 +1,14 @@
 local mod	= DBM:NewMod("Broodlord", "DBM-BWL", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20240203230300")
+mod:SetRevision("20240204192134")
 mod:SetCreatureID(12017)
 
 mod:SetModelID(14308)
 mod:RegisterCombat("yell", L.Pull)--L.Pull is backup for classic, since classic probably won't have ENCOUNTER_START to rely on and player regen never works for this boss
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS 23331 18670",
+	"SPELL_CAST_SUCCESS 23331 18670 25778",
 	"SPELL_AURA_APPLIED 24573",
 	"SPELL_AURA_REMOVED 24573"
 )
@@ -19,19 +19,22 @@ local warnBlastWave		= mod:NewSpellAnnounce(23331, 2)
 local warnKnockAway		= mod:NewSpellAnnounce(18670, 3)
 local warnMortal		= mod:NewTargetNoFilterAnnounce(24573, 2, nil, "Tank|Healer", 4)
 
-local timerMortal		= mod:NewTargetTimer(5, 24573, nil, "Tank|Healer", 4, 5, nil, DBM_COMMON_L.TANK_ICON)
 local timerBlastWaveCD	= mod:NewCDTimer(8.2, 23331, nil, nil, nil, 2, nil, nil, true) -- ~4s variance [8.20-12.53]. Added "keep" arg. (25m Onyxia [2024-02-03]@[22:41:24]) - "Blast Wave-23331-npc:12017-135 = pull:28.34, 11.30, 10.76, 8.39, 9.12, 8.20, 12.53, 10.57, 10.51, 12.82, 12.24, 8.67, 12.19"
+local timerKnockAwayCD	= mod:NewCDTimer(21.68, 18670, nil, nil, nil, 3, nil, nil, true) -- ~13s variance [21.68-34.93]. Added "keep" arg. (25m Onyxia [2024-02-03]@[22:41:24]) - "Knock Away-25778-npc:12017-135 = pull:26.83, 24.87, 21.68, 28.09, 24.92, 34.93"
+local timerMortal		= mod:NewTargetTimer(5, 24573, nil, "Tank|Healer", 4, 5, nil, DBM_COMMON_L.TANK_ICON)
 
 function mod:OnCombatStart(delay)
 	timerBlastWaveCD:Start(28.34-delay)
+	timerKnockAwayCD:Start(26.83-delay)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
 	if args.spellId == 23331 and args:IsSrcTypeHostile() then
 		warnBlastWave:Show()
 		timerBlastWaveCD:Start()
-	elseif args.spellId == 18670 then
+	elseif args.spellId == 18670 or args.spellId == 25778 then -- 02/02/2024: on Warmane it's using wrong id - https://www.warmane.com/bugtracker/report/120897
 		warnKnockAway:Show()
+		timerKnockAwayCD:Start()
 	end
 end
 
