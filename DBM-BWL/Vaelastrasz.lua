b63301007441a98c4e9ec4368e61f95eefaf4f8f
Zidras <10605951+Zidras@users.noreply.github.com>
2024-02-06 22:44:04 +0000

    BWL-Vaelastrasz: adjust timers

--- DBM-BWL/Vaelastrasz.lua
+++ DBM-BWL/Vaelastrasz.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Vaelastrasz", "DBM-BWL", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20240206211402")
+mod:SetRevision("20240206224337")
 mod:SetCreatureID(13020)
 mod:SetModelID(13992)
 mod:SetUsedIcons(8, 7, 6)
@@ -27,9 +27,9 @@ local specWarnAdrenalineOut	= mod:NewSpecialWarningMoveAway(18173, nil, nil, nil
 local yellAdrenaline		= mod:NewYell(18173, nil, false)
 local yellAdrenalineFades	= mod:NewShortFadesYell(18173)
 
-local timerAdrenalineCD		= mod:NewCDTimer(15.7, 18173, nil, nil, nil, 3)
+local timerAdrenalineCD		= mod:NewCDTimer(15.05, 18173, nil, nil, nil, 3) -- REVIEW! variance? (25m Onyxia: [2024-02-04]@[19:03:58]) - "Burning Adrenaline-23620-npc:13020-127 = pull:16.97, 15.05
 local timerAdrenaline		= mod:NewTargetTimer(20, 18173, nil, nil, nil, 3)
-local timerCombatStart		= mod:NewCombatTimer(43)
+local timerCombatStart		= mod:NewCombatTimer(44.08) -- [IEEU-YELL] - 770.50-726.42
 
 mod:AddSetIconOption("SetIconOnDebuffTarget2", 18173, true, false, {8, 7, 6})
 
@@ -37,7 +37,7 @@ mod.vb.debuffIcon = 8
 
 function mod:OnCombatStart(delay)
 	self.vb.debuffIcon = 8
-	timerAdrenalineCD:Start(15.7-delay)
+	timerAdrenalineCD:Start(16.97-delay)
 end
 
 function mod:SPELL_CAST_START(args)
@@ -93,7 +93,7 @@ function mod:SPELL_AURA_REMOVED(args)
 	end
 end
 
---"<??> [CHAT_MSG_MONSTER_YELL] Too late, friends! Nefarius' corruption has taken hold...I cannot...control myself.\r\n#Vaelastrasz the Corrupt###Omegal##0#0##0#7803#nil#0#false#false#false#false",
+--"<726.42 19:16:04> [CHAT_MSG_MONSTER_YELL] Too late, friends! Nefarius' corruption has taken hold...I cannot...control myself.\n#Vaelastrasz the Corrupt#####0#0##0#4985##0#"
 --"<8.85 19:59:36> [CHAT_MSG_MONSTER_YELL] I beg you, mortals - FLEE! Flee before I lose all sense of control! The black fire rages within my heart! I MUST- release it! #Vaelastrasz the Corrupt###Adornment##0#0##0#13862#nil#0#false#false#
 --"<28.25 19:59:55> [CHAT_MSG_MONSTER_YELL] FLAME! DEATH! DESTRUCTION! Cower, mortals before the wrath of Lord...NO - I MUST fight this! Alexstrasza help me, I MUST fight it! #Vaelastrasz the Corrupt###Adornment
 --"<38.98 20:00:06> [ENCOUNTER_START] 611#Vaelastrasz the Corrupt#9#40", -- [152]
