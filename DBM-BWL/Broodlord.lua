19e7132d77c9f0f788211f46ad0a3b818258e930
Zidras <10605951+Zidras@users.noreply.github.com>
2024-02-06 18:38:04 +0000

    BWL-Broodlord: minor refactors

--- DBM-BWL/Broodlord.lua
+++ DBM-BWL/Broodlord.lua
@@ -1,11 +1,10 @@
 local mod	= DBM:NewMod("Broodlord", "DBM-BWL", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20240206181240")
+mod:SetRevision("20240206183735")
 mod:SetCreatureID(12017)
-
 mod:SetModelID(14308)
-mod:RegisterCombat("yell", L.Pull)--L.Pull is backup for classic, since classic probably won't have ENCOUNTER_START to rely on and player regen never works for this boss
+mod:RegisterCombat("combat_yell", L.Pull)--L.Pull is backup for classic, since classic probably won't have ENCOUNTER_START to rely on and player regen never works for this boss
 
 mod:RegisterEventsInCombat(
 	"SPELL_CAST_SUCCESS 23331 18670 25778",
@@ -17,11 +16,11 @@ mod:RegisterEventsInCombat(
 --(ability.id = 18670 or ability.id = 23331 or ability.id = 24573) and type = "cast"
 local warnBlastWave		= mod:NewSpellAnnounce(23331, 2)
 local warnKnockAway		= mod:NewSpellAnnounce(18670, 3)
-local warnMortal		= mod:NewTargetNoFilterAnnounce(24573, 2, nil, "Tank|Healer", 4)
+local warnMortal		= mod:NewTargetNoFilterAnnounce(24573, 2, nil, "Tank|Healer", 3)
 
 local timerBlastWaveCD	= mod:NewCDTimer(8.2, 23331, nil, nil, nil, 2, nil, nil, true) -- ~7s variance [8.20-15.27]. Added "keep" arg. (25m Onyxia: [2024-02-03]@[22:41:24] || [2024-02-04]@[19:03:58]) - "Blast Wave-23331-npc:12017-135 = pull:28.34, 11.30, 10.76, 8.39, 9.12, 8.20, 12.53, 10.57, 10.51, 12.82, 12.24, 8.67, 12.19" || "Blast Wave-23331-npc:12017-135 = pull:24.91, 13.00, 8.95, 15.27, 11.03, 13.03
 local timerKnockAwayCD	= mod:NewCDTimer(21.68, 18670, nil, nil, nil, 3, nil, nil, true) -- ~13s variance [21.68-34.93]. Added "keep" arg. (25m Onyxia: [2024-02-03]@[22:41:24] || [2024-02-04]@[19:03:58]) - "Knock Away-25778-npc:12017-135 = pull:26.83, 24.87, 21.68, 28.09, 24.92, 34.93" || "Knock Away-25778-npc:12017-135 = pull:27.26, 23.44, 24.25
-local timerMortal		= mod:NewTargetTimer(5, 24573, nil, "Tank|Healer", 4, 5, nil, DBM_COMMON_L.TANK_ICON)
+local timerMortal		= mod:NewTargetTimer(5, 24573, nil, "Tank|Healer", 3, 5, nil, DBM_COMMON_L.TANK_ICON)
 
 function mod:OnCombatStart(delay)
 	timerBlastWaveCD:Start(24.9-delay)
@@ -29,24 +28,28 @@ function mod:OnCombatStart(delay)
 end
 
 function mod:SPELL_CAST_SUCCESS(args)
-	if args.spellId == 23331 and args:IsSrcTypeHostile() then
+	local spellId = args.spellId
+	if spellId == 23331 and args:IsSrcTypeHostile() then
 		warnBlastWave:Show()
 		timerBlastWaveCD:Start()
-	elseif args.spellId == 18670 or args.spellId == 25778 then -- 02/02/2024: on Warmane it's using wrong id - https://www.warmane.com/bugtracker/report/120897
+	elseif spellId == 18670 or spellId == 25778 then -- 02/02/2024: on Warmane it's using wrong id - https://www.warmane.com/bugtracker/report/120897
 		warnKnockAway:Show()
 		timerKnockAwayCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED(args)
-	if args.spellId == 24573 and args:IsDestTypePlayer() then
-		warnMortal:Show(args.destName)
-		timerMortal:Start(args.destName)
+	local spellId = args.spellId
+	if spellId == 24573 and args:IsDestTypePlayer() then
+		local destName = args.destName
+		warnMortal:Show(destName)
+		timerMortal:Start(destName)
 	end
 end
 
 function mod:SPELL_AURA_REMOVED(args)
-	if args.spellId == 24573 and args:IsDestTypePlayer() then
+	local spellId = args.spellId
+	if spellId == 24573 and args:IsDestTypePlayer() then
 		timerMortal:Stop(args.destName)
 	end
 end
