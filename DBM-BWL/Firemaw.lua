b2c2cb582b34448a98f895db6c67fccd2a8bcb13
Zidras <10605951+Zidras@users.noreply.github.com>
2024-02-06 21:52:39 +0000

    BWL-Firemaw: adjust timers

--- DBM-BWL/Firemaw.lua
+++ DBM-BWL/Firemaw.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Firemaw", "DBM-BWL", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20240206185452")
+mod:SetRevision("20240206215210")
 mod:SetCreatureID(11983)
 mod:SetModelID(6377)
 mod:RegisterCombat("combat")
@@ -16,12 +16,12 @@ local warnWingBuffet		= mod:NewCastAnnounce(23339, 2)
 local warnShadowFlame		= mod:NewCastAnnounce(22539, 2)
 local warnFlameBuffet		= mod:NewStackAnnounce(23341, 3)
 
-local timerWingBuffet		= mod:NewCDTimer(31, 23339, nil, nil, nil, 2)
-local timerShadowFlameCD	= mod:NewCDTimer(14, 22539, nil, false)--14-21
+local timerWingBuffet		= mod:NewNextTimer(30, 23339, nil, nil, nil, 2) -- Fixed timer. (25m Onyxia: [2024-02-04]@[19:03:58]) - "Wing Buffet-23339-npc:11983-134 = pull:29.97, 30.03, 30.02, 30.02
+local timerShadowFlameCD	= mod:NewCDTimer(12.68, 22539, nil, false, nil, 5, nil, nil, true) -- ~4s variance [12.68-16.58]. Added "keep" arg. (25m Onyxia: [2024-02-04]@[19:03:58]) - "Shadow Flame-22539-npc:11983-134 = pull:16.71, 16.58, 15.07, 15.49, 14.93, 16.38, 12.68
 
 function mod:OnCombatStart(delay)
-	timerShadowFlameCD:Start(18-delay)
-	timerWingBuffet:Start(30-delay)
+	timerShadowFlameCD:Start(16.7-delay)
+	timerWingBuffet:Start(-delay)
 end
 
 function mod:SPELL_CAST_START(args)
