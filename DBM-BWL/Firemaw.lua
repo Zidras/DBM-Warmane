b7913842465f45e968907571dfe9d05c5341f73b
Zidras <10605951+Zidras@users.noreply.github.com>
2024-02-06 18:58:20 +0000

    BWL-Firemaw: minor refactors

--- DBM-BWL/Firemaw.lua
+++ DBM-BWL/Firemaw.lua
@@ -1,9 +1,8 @@
 local mod	= DBM:NewMod("Firemaw", "DBM-BWL", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220518110528")
+mod:SetRevision("20240206185452")
 mod:SetCreatureID(11983)
-
 mod:SetModelID(6377)
 mod:RegisterCombat("combat")
 
@@ -15,9 +14,9 @@ mod:RegisterEventsInCombat(
 --(ability.id = 23339 or ability.id = 22539) and type = "begincast" or ability.id = 23341 and type = "cast"
 local warnWingBuffet		= mod:NewCastAnnounce(23339, 2)
 local warnShadowFlame		= mod:NewCastAnnounce(22539, 2)
-local warnFlameBuffet		= mod:NewCountAnnounce(23341, 3, nil, nil, DBM_CORE_L.AUTO_ANNOUNCE_OPTIONS.stack:format(23341))
+local warnFlameBuffet		= mod:NewStackAnnounce(23341, 3)
 
-local timerWingBuffet		= mod:NewCDTimer(31, 23339, nil, nil, nil, 2)--Verified on classic 31-36
+local timerWingBuffet		= mod:NewCDTimer(31, 23339, nil, nil, nil, 2)
 local timerShadowFlameCD	= mod:NewCDTimer(14, 22539, nil, false)--14-21
 
 function mod:OnCombatStart(delay)
@@ -25,18 +24,20 @@ function mod:OnCombatStart(delay)
 	timerWingBuffet:Start(30-delay)
 end
 
-function mod:SPELL_CAST_START(args)--did not see ebon use any of these abilities
-	if args.spellId == 23339 then
+function mod:SPELL_CAST_START(args)
+	local spellId = args.spellId
+	if spellId == 23339 then
 		warnWingBuffet:Show()
 		timerWingBuffet:Start()
-	elseif args.spellId == 22539 then
+	elseif spellId == 22539 then
 		warnShadowFlame:Show()
 		timerShadowFlameCD:Start()
 	end
 end
 
 function mod:SPELL_AURA_APPLIED_DOSE(args)
-	if args.spellId == 23341 and args:IsPlayer() then
+	local spellId = args.spellId
+	if spellId == 23341 and args:IsPlayer() then
 		local amount = args.amount or 1
 		if (amount >= 4) and (amount % 2 == 0) then--Starting at 4, every even amount warn stack
 			warnFlameBuffet:Show(amount)
