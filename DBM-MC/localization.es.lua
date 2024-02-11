401f2634b876055fb78a87e3628741b156058fcc
Zidras <10605951+Zidras@users.noreply.github.com>
2024-02-11 23:49:22 +0000

    MC: fix locales

--- DBM-MC/localization.es.lua
+++ DBM-MC/localization.es.lua
@@ -117,7 +117,7 @@ L:SetOptionLocalization({
 })
 
 L:SetMiscLocalization({
-	Submerge	= "¡SIERVOS, A MÍ! ¡DEFENDED A VUESTRO MAESTRO!",
+	Submerge	= "¡AVANZAD, MIS SIRVIENTES! ¡DEFENDED A VUESTRO MAESTRO!",
 	Submerge2	= "¡NO PODÉIS VENCER A LA LLAMA VIVIENTE! ¡VENID, ESCLAVOS DEL FUEGO! ¡AVANZAD, CRIATURAS DEL ODIO! ¡VUESTRO MAESTRO OS LLAMA!",
 	Pull		= "¡Crías imprudentes! ¡Os habéis precipitado hasta vuestra propia muerte! ¡Ahora mirad, el maestro se agita!"
 })
