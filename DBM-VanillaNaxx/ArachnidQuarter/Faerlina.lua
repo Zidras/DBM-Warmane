d7725031f2b9a49865b5fdbe34fe0646102234ad
Zidras <10605951+Zidras@users.noreply.github.com>
2024-07-14 15:33:07 +0100

    VanillaNaxx-Faerlina: adjust enrage

--- DBM-VanillaNaxx/ArachnidQuarter/Faerlina.lua
+++ DBM-VanillaNaxx/ArachnidQuarter/Faerlina.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Faerlina-Vanilla", "DBM-VanillaNaxx", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20240709230003")
+mod:SetRevision("20240714153159")
 mod:SetCreatureID(15953)
 
 mod:RegisterCombat("combat_yell", L.Pull)
@@ -22,8 +22,8 @@ local specWarnEnrage		= mod:NewSpecialWarningDefensive(28131, nil, nil, nil, 3, 
 local specWarnGTFO			= mod:NewSpecialWarningGTFO(28794, nil, nil, nil, 1, 8)
 
 local timerEmbrace			= mod:NewBuffActiveTimer(30, 28732, nil, nil, nil, 6)
-local timerEnrage			= mod:NewCDTimer(60, 28131, nil, nil, nil, 6)
-local timerPoisonVolleyCD	= mod:NewCDTimer(8.13, 28796, nil, nil, nil, 5) -- ~1s variance [8.13-9.58] (Onyxia: [2024-07-08]@[19:04:03]) - "Poison Bolt Volley-28796-npc:15953-798 = pull:13.51, 8.13, 9.58, 8.98, 8.84, 9.50, 8.88, 8.26, 9.01"
+local timerEnrage			= mod:NewCDTimer(63.26, 28131, nil, nil, nil, 6)
+local timerPoisonVolleyCD	= mod:NewCDTimer(8.13, 28796, nil, nil, nil, 5) -- ~2s variance [8.13-9.96] (Onyxia: [2024-07-08]@[19:04:03] || [2024-07-13]@[13:46:21]) - "Poison Bolt Volley-28796-npc:15953-798 = pull:13.51, 8.13, 9.58, 8.98, 8.84, 9.50, 8.88, 8.26, 9.01" || "Poison Bolt Volley-28796-npc:15953-798 = pull:13.80, 9.30, 9.96, 9.83, 9.44, 9.89, 9.76, 8.58, 8.41"
 
 mod.vb.enraged = false
 
