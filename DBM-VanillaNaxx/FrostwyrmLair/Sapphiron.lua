f9f108d9e71191e25d66dfad3cd17610113a4473
Zidras <10605951+Zidras@users.noreply.github.com>
2024-07-14 11:58:52 +0100

    VanillaNaxx-Sapphiron: rework module

--- DBM-VanillaNaxx/FrostwyrmLair/Sapphiron.lua
+++ DBM-VanillaNaxx/FrostwyrmLair/Sapphiron.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Sapphiron-Vanilla", "DBM-VanillaNaxx", 5)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20240701222429")
+mod:SetRevision("20240714115710")
 mod:SetCreatureID(15989)
 mod:SetMinSyncRevision(20220904000000)
 
@@ -9,11 +9,13 @@ mod:RegisterCombat("combat")
 mod:SetModelScale(0.1)
 
 mod:RegisterEventsInCombat(
---	"SPELL_CAST_START 28524",
+	"SPELL_CAST_START 28524",
 	"SPELL_CAST_SUCCESS 28542 55665 28560 55696",
 	"SPELL_AURA_APPLIED 28522 28547 55699",
-	"CHAT_MSG_RAID_BOSS_EMOTE",
-	"UNIT_HEALTH boss1"
+	"CHAT_MSG_RAID_BOSS_EMOTE", -- 2024/07/10 it was confirmed to be left out of the script on purpose, to keep it Vanilla.
+	"UNIT_HEALTH boss1",
+	"UNIT_SPELLCAST_SUCCEEDED boss1",
+	"UNIT_TARGET boss1"
 )
 
 --TODO, verify SPELL_CAST_START on retail to switch to it over emote, same as classicc era was done
@@ -33,10 +35,10 @@ local warnBlizzard		= mod:NewSpellAnnounce(28560, 4)
 
 local specWarnBlizzard	= mod:NewSpecialWarningGTFO(28547, nil, nil, nil, 1, 8)
 
-local timerDrainLife	= mod:NewCDTimer(24, 28542, nil, nil, nil, 3, nil, DBM_COMMON_L.CURSE_ICON) -- (25man Lordaeron 2022/09/02) - 24.0
-local timerAirPhase		= mod:NewTimer(60, "TimerAir", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp", nil, nil, 6)
-local timerBlizzard		= mod:NewNextTimer(7, 28560, nil, nil, nil, 3)
-local timerTailSweep	= mod:NewNextTimer(9, 55696, nil, nil, nil, 2)
+local timerDrainLife	= mod:NewCDTimer(24, 28542, nil, nil, nil, 3, nil, DBM_COMMON_L.CURSE_ICON) -- (Onyxia PTR: [2024-07-13]@[15:10:23]) - "Life Drain-28542-npc:15989-1847 = pull:12.01/[Stage 1/0.00] 12.01, 23.99, Stage 2/8.99,
+local timerAirPhase		= mod:NewTimer(67, "TimerAir", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendUnBurrow.blp", nil, nil, 6)
+local timerBlizzard		= mod:NewNextTimer(20, 28560, nil, nil, nil, 3) -- REVIEW!
+local timerTailSweep	= mod:NewNextTimer(9, 55696, nil, nil, nil, 2) -- (Onyxia PTR: [2024-07-13]@[15:10:23]) - "Tail Sweep-55697-npc:15989-1847 = pull:9.01/[Stage 1/0.00] 9.01, 9.01, 8.99, 9.04, Stage 2/8.94,...
 
 -- Stage Two (Air Phase)
 mod:AddTimerLine(DBM_CORE_L.SCENARIO_STAGE:format(2))
@@ -46,8 +48,9 @@ local warnIceBlock		= mod:NewTargetAnnounce(28522, 2)
 local specWarnDeepBreath= mod:NewSpecialWarningSpell(28524, nil, nil, nil, 1, 2)
 local yellIceBlock		= mod:NewYell(28522)
 
-local timerLanding		= mod:NewTimer(24.2, "TimerLanding", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp", nil, nil, 6)
-local timerIceBlast		= mod:NewCastTimer(8, 28524, nil, nil, nil, 2, DBM_COMMON_L.DEADLY_ICON)
+local timerLanding		= mod:NewTimer(30, "TimerLanding", "Interface\\AddOns\\DBM-Core\\textures\\CryptFiendBurrow.blp", nil, nil, 6)
+local timerIceBlast		= mod:NewCastTimer(7, 28524, nil, nil, nil, 2, DBM_COMMON_L.DEADLY_ICON)
+local timerIcebolt		= mod:NewNextTimer(3, 28522, nil, nil, nil, 2)
 
 mod:AddRangeFrameOption("12")
 
@@ -55,21 +58,45 @@ mod:AddRangeFrameOption("12")
 --local noTargetTime = 0
 local warned_lowhp = false
 mod.vb.isFlying = false
+mod.vb.iceboltCount = 0
+local iceboltSpellName = DBM:GetSpellInfo(28522)
 
 --[[local function resetIsFlying(self)
 	self.vb.isFlying = false
 end]]
 
+-- "<0.00 19:29:35> [DBM_Pull] Sapphiron-Vanilla#0#nil#99.647332005526#", -- [1]
+-- "<0.00 19:29:35> [DBM_SetStage] Sapphiron-Vanilla#1#1#", -- [2]
+-- "<44.99 19:30:20> [UNIT_TARGET] -boss1:Sapphiron- [CanAttack:1#Exists:1#IsVisible:1#ID:15989#GUID:0xF130003E75000A74#Classification:worldboss#Health:2310647] - Target: ??#TargetOfTarget: ??", -- [3]
+-- "<75.76 19:30:51> [UNIT_TARGET] -boss1:Sapphiron- [CanAttack:1#Exists:1#IsVisible:1#ID:15989#GUID:0xF130003E75000A74#Classification:worldboss#Health:2244077] - Target: Name#TargetOfTarget: Sapphiron", -- [4]
+-- "<142.67 19:31:57> [UNIT_TARGET] -boss1:Sapphiron- [CanAttack:1#Exists:1#IsVisible:1#ID:15989#GUID:0xF130003E75000A74#Classification:worldboss#Health:1940109] - Target: ??#TargetOfTarget: ??", -- [5]
+-- "<173.89 19:32:29> [UNIT_TARGET] -boss1:Sapphiron- [CanAttack:1#Exists:1#IsVisible:1#ID:15989#GUID:0xF130003E75000A74#Classification:worldboss#Health:1672129] - Target: Name#TargetOfTarget: Sapphiron", -- [6]
+-- "<193.60 19:32:48> [INSTANCE_ENCOUNTER_ENGAGE_UNIT] Fake Args:#boss1#nil#nil#nil#??#1#nil#normal#0#boss2#nil#nil#nil#??#1#nil#normal#0#boss3#nil#nil#nil#??#1#nil#normal#0#boss4#nil#nil#nil#??#1#nil#normal#0#boss5#nil#nil#nil#??#1#nil#normal#0#Real Args:#", -- [7]
+-- "<193.61 19:32:48> [UNIT_TARGET] -target:Sapphiron- [CanAttack:1#Exists:1#IsVisible:1#ID:15989#GUID:0xF130003E75000A74#Classification:worldboss#Health:1] - Target: ??#TargetOfTarget: ??", -- [8]
+-- "<193.61 19:32:48> [DBM_Kill] Sapphiron-Vanilla#", -- [9]
+
+local function Flying(self)
+	self:SetStage(2)
+	self.vb.isFlying = true
+	timerDrainLife:Cancel()
+	timerAirPhase:Cancel()
+	warnAirPhaseNow:Show()
+	timerLanding:Start() -- this will be innacurate as it does not account for boss travel distance to center. Updated on Frost Breath timer
+end
+
 local function Landing(self)
 	self:SetStage(1)
+	self.vb.isFlying = false
+	self.vb.iceboltCount = 0
 	warnLanded:Show()
 	warnDrainLifeSoon:Schedule(5)
-	timerDrainLife:Start(10.5)
-	warnAirPhaseSoon:Schedule(50)
-	timerAirPhase:Start()
+	timerDrainLife:Start(10.5) -- REVIEW!
+	warnAirPhaseSoon:Schedule(62)
+	timerAirPhase:Start() -- 67s after first landing onwards
+	self:Schedule(67, Flying, self)
 	if self.Options.RangeFrame then
 		DBM.RangeCheck:Hide()
-		self:Schedule(59, DBM.RangeCheck.Show, DBM.RangeCheck, 12)
+		self:Schedule(65, DBM.RangeCheck.Show, DBM.RangeCheck, 12)
 	end
 end
 
@@ -78,15 +105,17 @@ function mod:OnCombatStart(delay)
 	warned_lowhp = false
 	self:SetStage(1)
 	self.vb.isFlying = false
+	self.vb.iceboltCount = 0
 	warnDrainLifeSoon:Schedule(6.5-delay)
-	timerDrainLife:Start(12-delay) -- (25man Lordaeron 2022/09/02) - 12.0
-	timerBlizzard:Start(6.1-delay) -- REVIEW! ~3s variance? (25man Lordaeron 2022/09/02 || 25man Lordaeron 2022/10/16) - 8.8 || 6.1
+	timerDrainLife:Start(12-delay)
+	timerBlizzard:Start(15-delay) -- REVIEW!
 	timerTailSweep:Start(-delay)
-	warnAirPhaseSoon:Schedule(38.4-delay)
-	timerAirPhase:Start(48.4-delay) -- REVIEW! ~0.1s variance? (25man Lordaeron 2022/09/02) - 48.4
+	warnAirPhaseSoon:Schedule(40-delay)
+	timerAirPhase:Start(45-delay) -- 45s since Combat Start
+	self:Schedule(45, Flying, self)
 	berserkTimer:Start(-delay)
 	if self.Options.RangeFrame then
-		self:Schedule(46-delay, DBM.RangeCheck.Show, DBM.RangeCheck, 12)
+		self:Schedule(43-delay, DBM.RangeCheck.Show, DBM.RangeCheck, 12)
 	end
 --[[self:RegisterOnUpdateHandler(function(self, elapsed)
 		if not self:IsInCombat() then return end
@@ -135,17 +164,16 @@ function mod:SPELL_AURA_APPLIED(args)
 	end
 end
 
---[[
+
 function mod:SPELL_CAST_START(args)
-	--if args:IsSpellID(28524, 29318) then--NEEDS verification before deployed
+	if args.spellId == 28524 then
 		timerIceBlast:Start()
-		timerLanding:Update(16.3, 28.5)--Probably not even needed, if base timer is more accurate
-		self:Schedule(12.2, Landing, self)
-		warnDeepBreath:Show()
-		warnDeepBreath:Play("findshelter")
+		timerLanding:Update(11, 30) -- 11s after Frost Breath cast start
+		self:Schedule(11, Landing, self)
+		specWarnDeepBreath:Show()
+		specWarnDeepBreath:Play("findshelter")
 	end
 end
---]]
 
 function mod:SPELL_CAST_SUCCESS(args)
 	local spellId = args.spellId
@@ -154,6 +182,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 		warnDrainLifeSoon:Schedule(18.5)
 		timerDrainLife:Start()
 	elseif spellId == 28560 then
+		DBM:AddMsg("Sapphiron Blizzard SPELL_CAST_SUCCESS implemented on Warmane Onyxia server script. Notify Zidras on Discord or GitHub")
 		warnBlizzard:Show()
 		timerBlizzard:Start()
 	elseif spellId == 55696 then
@@ -163,12 +192,14 @@ end
 
 function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 	if msg == L.EmoteBreath or msg:find(L.EmoteBreath) then
+		DBM:AddMsg("Sapphiron Breath emote implemented on Warmane Onyxia server script. Notify Zidras on Discord or GitHub")
 --		self:SendSync("DeepBreath") -- this does not need syncing and spam comms
-		timerIceBlast:Start()
-		timerLanding:Update(13.5) -- 8s until breath + 3.5s until emote + ~2s until UNIT_TARGET
+--		timerIceBlast:Start()
+--		timerLanding:Update(13.5) -- 8s until breath + 3.5s until emote + ~2s until UNIT_TARGET
 		specWarnDeepBreath:Show()
 		specWarnDeepBreath:Play("findshelter")
 	elseif msg == L.AirPhase or msg:find(L.AirPhase) then
+		DBM:AddMsg("Sapphiron Air Phase emote implemented on Warmane Onyxia server script. Notify Zidras on Discord or GitHub")
 		self:SetStage(2)
 		self.vb.isFlying = true
 		timerDrainLife:Cancel()
@@ -176,10 +207,9 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 		warnAirPhaseNow:Show()
 		timerLanding:Start()
 	elseif msg == L.LandingPhase or msg:find(L.LandingPhase) then
+		DBM:AddMsg("Sapphiron Landing Phase emote implemented on Warmane Onyxia server script. Notify Zidras on Discord or GitHub")
+		self:SetStage(1)
 		self.vb.isFlying = false
-		self:RegisterShortTermEvents(
-			"UNIT_TARGET boss1"
-		)
 	end
 end
 
@@ -191,20 +221,40 @@ function mod:UNIT_HEALTH(uId)
 	end
 end
 
-function mod:UNIT_TARGET(uId)
-	-- Apparently there's a delay after emote where boss is actually engaged (TC has 3.5s). From S3 Naxx videos this is somewhat still accurate, so check when boss retargets raid member, which is when land phase script restarts
-	if UnitExists(uId.."target") then
-		Landing(self)
-		self:UnregisterShortTermEvents()
+function mod:UNIT_SPELLCAST_SUCCEEDED(_, spellName)
+	if spellName == iceboltSpellName then -- Icebolt
+		self.vb.iceboltCount = self.vb.iceboltCount + 1
+		if self.vb.iceboltCount < 5 then -- 5 Icebolts on each Flight phase
+			timerIcebolt:Start()
+		end
 	end
 end
 
---[[function mod:OnSync(event)
-	if event == "DeepBreath" then
+function mod:UNIT_TARGET(uId)
+	-- REVIEW! Check if this is accurate to rely on
+	if UnitExists(uId.."target") then
+		DBM:Debug("UNIT_TARGET event fired, flying status: " .. tostring(self.vb.isFlying or "nil"))
+		if self.vb.isFlying then
+			self:SendSync("SapphironLanded") -- Sync landing with raid since UNIT_TARGET:boss1 event requires boss to be target/focus, which not all members do
+		end
+	else
+		DBM:Debug("UNIT_TARGET found no boss target, flying status: " .. tostring(self.vb.isFlying or "nil"))
+		if not self.vb.isFlying then
+			self:SendSync("SapphironFlying") -- Sync airphase with raid since UNIT_TARGET:boss1 event requires boss to be target/focus, which not all members do
+		end
+	end
+end
+
+function mod:OnSync(msg)
+	--[[if msg == "DeepBreath" then
 		timerIceBlast:Start()
 		timerLanding:Update(14)
 		self:Schedule(14.5, Landing, self)
 		specWarnDeepBreath:Show()
-		specWarnDeepBreath:Play("findshelter")
+		specWarnDeepBreath:Play("findshelter")]]
+	if msg == "SapphironFlying" and self.vb.phase == 1 then
+		Flying()
+	elseif msg == "SapphironLanded" and self.vb.phase == 2 then
+		Landing()
 	end
-end]]
+end
