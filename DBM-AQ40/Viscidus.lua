25722e4e08bb56056b4ef957514c34fe7782a574
Zidras <10605951+Zidras@users.noreply.github.com>
2024-07-16 23:25:26 +0100

    AQ40-Viscidus: adjust Poison Bolt Volley CD timer

--- DBM-AQ40/Viscidus.lua
+++ DBM-AQ40/Viscidus.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Viscidus", "DBM-AQ40", 1)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20240707194121")
+mod:SetRevision("20240716232440")
 mod:SetCreatureID(15299)
 
 mod:SetModelID(15299)
@@ -20,7 +20,7 @@ local warnShatter				= mod:NewAnnounce("WarnShatter", 2, 12982)
 
 local specWarnGTFO				= mod:NewSpecialWarningGTFO(25989, nil, nil, nil, 1, 8)
 
-local timerPoisonBoltVolleyCD	= mod:NewCDCountTimer(10.88, 25991, nil, nil, nil, 2, nil, DBM_COMMON_L.POISON_ICON, true) -- ~5s variance [10.88-14.70]. Added "keep" arg (Onyxia: 25N [2024-07-05]@[18:31:25]) - "Poison Bolt Volley-25991-npc:15299-773 = pull:7.78, 11.25, 10.88, 51.85, 14.70, 13.66"
+local timerPoisonBoltVolleyCD	= mod:NewCDCountTimer(10.13, 25991, nil, nil, nil, 2, nil, DBM_COMMON_L.POISON_ICON, true) -- ~5s variance [10.13-14.98]. Added "keep" arg (Onyxia: 25N [2024-07-05]@[18:31:25] || PTR: [2024-07-16]@[22:41:16]) - "Poison Bolt Volley-25991-npc:15299-610 = pull:13.42, 14.33, 13.24, 13.18, 10.50, 12.49, 10.18, 10.17, 10.46, 11.12, 13.84, 11.87, 13.18, 13.47, 10.61, 13.15, 12.13, 14.30, 13.97, 12.19, 14.32, 13.99, 10.55, 10.72, 13.85, 10.21, 12.42, 10.13, 14.38, 12.72, 12.41, 14.32, 13.27, 13.75, 12.15, 14.29, 13.68, 11.05, 13.90, 10.48, 14.25, 10.92, 14.90, 11.64, 12.02, 12.54, 11.47, 12.89, 14.15, 14.40, 10.78, 10.68, 13.01, 13.17, 14.98, 10.71, 12.79, 11.44"
 
 mod.vb.volleyCount = 0
 
