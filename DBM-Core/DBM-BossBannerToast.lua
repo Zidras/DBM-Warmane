bfc76f0af2c1b24170e90694cfecce87b73d4930
Zidras <10605951+Zidras@users.noreply.github.com>
2024-11-09 15:46:58 +0000

    BossBanner: show Loot animation if pending loot
    Retail did not account for receiving Loot on the end of KILL animation, or that exceeds BB_MAX_LOOT. It would always require a new ENCOUNTER_LOOT_RECEIVED to iterate through pendingLoot.
    This workaround now checks on the end of BossBanner animation if pendingLoot is populated in order to do another round of Loot animation with remaining loot.

--- DBM-Core/DBM-BossBannerToast.lua
+++ DBM-Core/DBM-BossBannerToast.lua
@@ -292,7 +292,12 @@ local function TopBannerManager_BannerFinished()
 --		TopBannerMgr.currentBanner.frame:PlayBanner(TopBannerMgr.currentBanner.data) -- Needs investigation, crashes the client!
 		DBM:Schedule(0, TopBannerMgr.currentBanner.frame.PlayBanner, TopBannerMgr.currentBanner.frame, TopBannerMgr.currentBanner.data)
 	else
-		TopBannerMgr.currentBanner = nil
+		if next(TopBannerMgr.currentBanner.frame.pendingLoot) then -- has pending loot, send next
+			TopBannerMgr.currentBanner.data.mode = "LOOT" -- change mode to LOOT, since KILL would show kill banner again
+			DBM:Schedule(0, TopBannerMgr.currentBanner.frame.PlayBanner, TopBannerMgr.currentBanner.frame, TopBannerMgr.currentBanner.data)
+		else
+			TopBannerMgr.currentBanner = nil
+		end
 	end
 end
 
@@ -1143,7 +1148,7 @@ local function BossBanner_AnimLootInsert(self, entry)
 			return true
 		end
 	end
-	if  self.lootShown > 0 then
+	if self.lootShown > 0 then
 		entry.duration = 4
 	else
 		entry.duration = 0
