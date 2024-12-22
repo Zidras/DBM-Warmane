75105da54acb5e647d37cd0cc68479bc1ac2764e
5Buttons <tim.diersmann@outlook.de>
2024-12-22 18:32:28 +0100

    Add files via upload

--- DBM-Hyjal/Archimonde.lua
+++ DBM-Hyjal/Archimonde.lua
@@ -10,25 +10,35 @@ mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 31972",
-	"SPELL_CAST_START 31970 32014"
+	"SPELL_AURA_REMOVED 31972",
+	"SPELL_CAST_START 31970 32014",
+	"CHAT_MSG_MONSTER_YELL"
 )
 
 local warnGrip			= mod:NewTargetNoFilterAnnounce(31972, 3, nil, "RemoveMagic")--Magic on retail, but I think a curse in TBC
 local warnBurst			= mod:NewTargetNoFilterAnnounce(32014, 3)
 local warnFear			= mod:NewSpellAnnounce(31970, 3)
+local warnDoomfire		= mod:NewSpellAnnounce(31943, 3)
 
 local specWarnBurst		= mod:NewSpecialWarningYou(32014, nil, nil, nil, 3, 2)
 local yellBurst			= mod:NewYell(32014)
 
-local timerFearCD		= mod:NewCDTimer(41, 31970, nil, nil, nil, 2)
---local timerGripCD		= mod:NewCDTimer(6, 31972, nil, "RemoveMagic", nil, 3, nil, DBM_COMMON_L.MAGIC_ICON)
+local timerNextFear		= mod:NewNextTimer(42, 31970, nil, nil, nil, 2)
+
+local timerNextDoomfire		= mod:NewNextTimer(8, 31943, nil, nil, nil, 2)
 
 local berserkTimer		= mod:NewBerserkTimer(600)
 
 mod:AddSetIconOption("BurstIcon", 32014, true, false, {8})
 
+mod:AddSetIconOption("GripIcon", 31972, false, false, {7})
+
 function mod:BurstTarget(targetname)
-	if not targetname then return end
+
+	if not targetname then 
+		return 
+	end
+	
 	if targetname == UnitName("player") then
 		specWarnBurst:Show()
 		specWarnBurst:Play("targetyou")
@@ -40,24 +50,53 @@ function mod:BurstTarget(targetname)
 		self:SetIcon(targetname, 8, 5)
 	end
 end
-
+	
 function mod:OnCombatStart(delay)
-	timerFearCD:Start(40-delay)
-	berserkTimer:Start(-delay)
-end
 
+	berserkTimer:Start(-delay)
+	
+	timerNextFear:Start(40-delay)
+	timerNextDoomfire:Start(-delay)
+
+	timerNextDoomfire:UpdateName("Doomfire CD")
+end
 
 function mod:SPELL_AURA_APPLIED(args)
 	if args.spellId == 31972 then
 		warnGrip:Show(args.destName)
+
+		if self.Options.GripIcon then 
+			self:SetIcon(args.destName, 7)	
+		end
 	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+
+	if args.spellId == 31972 then 
+		if self.Options.GripIcon then	
+			self:SetIcon(args.destName,0) 
+		end
+	end	
 end
 
 function mod:SPELL_CAST_START(args)
 	if args.spellId == 31970 then
 		warnFear:Show()
-		timerFearCD:Start()
+		timerNextFear:Start()
 	elseif args.spellId == 32014 then
 		self:BossTargetScanner(17968, "BurstTarget", 0.05, 10)
+		
+		timerNextFear:AddTime(5)
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+
+	if msg == L.ArchimondeDoomfire1
+		or msg == L.ArchimondeDoomfire2
+	then
+		warnDoomfire:Show()
+		timerNextDoomfire:Start()
 	end
 end
