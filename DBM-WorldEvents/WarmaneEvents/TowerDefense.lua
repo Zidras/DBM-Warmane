c736ecdbeed32d0332033b6ef1c6590b4b3ed642
Zidras <10605951+Zidras@users.noreply.github.com>
2024-12-30 19:46:33 +0000

    Warmane-TowerDefense: add Azuregos boss

--- DBM-WorldEvents/WarmaneEvents/TowerDefense.lua
+++ DBM-WorldEvents/WarmaneEvents/TowerDefense.lua
@@ -1,24 +1,37 @@
 local mod	= DBM:NewMod("WarmaneTowerDefense", "DBM-WorldEvents", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20241230183312")
+mod:SetRevision("20241230194607")
 mod.noStatistics = true
 
 mod:RegisterEvents(
 	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
 
+-- General
 local warnBossNow					= mod:NewSpellAnnounce(31315, 1)
 
+local timerToRessurect				= mod:NewNextTimer(30, 72423, nil, nil, nil, 6)
+local timerCombatStart				= mod:NewCombatTimer(45)
+
+-- Trash
 local specWarnSpellReflectDispel	= mod:NewSpecialWarningDispel(36096, "MagicDispeller", nil, nil, 1, 2)
 local specWarnHandOfProtectionDispel= mod:NewSpecialWarningDispel(66009, "ImmunityDispeller", nil, nil, 1, 2)
--- Shade of Aran
+
+-- Bosses
+-- Shade of Aran (400024)
 local specWarnCounterspellStopCast	= mod:NewSpecialWarningCast(31999, "SpellCaster", nil, nil, 1, 2) -- TBC spellId
 local specWarnIceBlastRun			= mod:NewSpecialWarningRun(73775, "Melee", nil, nil, 4, 2) -- TBC spellId
 local specWarnLivingBombMoveAway	= mod:NewSpecialWarningMoveAway(73061, "Melee", nil, nil, 1, 2) -- TBC spellId
 
-local timerToRessurect				= mod:NewNextTimer(30, 72423, nil, nil, nil, 6)
-local timerCombatStart				= mod:NewCombatTimer(45)
+-- Azuregos (400052)
+local warnReflection				= mod:NewSpellAnnounce(22067, 2)
+
+local specWarnChillDispel			= mod:NewSpecialWarningDispel(21098, "MagicDispeller", nil, nil, 1, 2)
+
+local timerTailSweep				= mod:NewVarTimer("v15-20", 15847, nil, nil, nil, 2) -- 5s variance [15-20] (Lordaeron Horde [2024-12-27]@[13:17:37]) - "Tail Sweep-15847-npc:6836-432 = pull:1152.8, 18.2, 16.3, 18.7, 16.7, 16.3, 16.3, 17.3, 19.6, 16.4"
+local timerFrostBreath				= mod:NewVarTimer("v15-20", 21099, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON) -- 5s variance [15-20] (Lordaeron Horde [2024-12-27]@[13:17:37]) - "Frost Breath-21099-npc:6836-432 = pull:1148.1, 18.7, 16.1, 18.6, 15.9, 19.5, 16.5, 15.2, 20.0, 15.8"
+local timerNextChill				= mod:NewNextTimer(15, 21098, nil, nil, nil, 2) -- (Lordaeron Horde [2024-12-27]@[13:17:37]) - "Chill-21098-npc:6836-432 = pull:1160.1[+41], 14.7[+10], 0.1[+20], 15.0[+38], 14.9[+26], 15.0[+18], 15.0[+21], 14.9[+21], 15.0[+18], 14.9[+8], 0.1[+18], 14.9, 0.1[+4], 14.9"
 
 mod:AddRangeFrameOption(15, 73775) -- TBC spellId
 mod:RemoveOption("HealthFrame")
@@ -50,6 +63,10 @@ function mod:SPELL_CAST_START(args)
 
 			DBM.RangeCheck:SetBossRange(15, self:GetBossUnitByCreatureId(400024)) -- Shade of Aran
 		end
+	elseif spellId == 15847 then -- Tail Sweep
+		timerTailSweep:Start()
+	elseif spellId == 21099 then -- Frost Breath
+		timerFrostBreath:Start()
 	end
 end
 
@@ -64,6 +81,12 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif spellId == 73061 and args:IsDestTypePlayer() then -- Living Bomb
 		specWarnLivingBombMoveAway:Show()
 		specWarnLivingBombMoveAway:Play("runout")
+	elseif spellId == 21098 and args:IsDestTypePlayer() then -- Chill
+		timerNextChill:Start()
+		specWarnChillDispel:Show()
+		specWarnChillDispel:Play("dispelnow")
+	elseif spellId == 22067 then -- Reflection
+		warnReflection:Show()
 	end
 end
 
@@ -77,16 +100,14 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 			warnBossNow:Show()
 		end
 		self:RegisterShortTermEvents(
-			"SPELL_CAST_START 31999 73775",
-			"SPELL_AURA_APPLIED 36096 66009 73061"
+			"SPELL_CAST_START 31999 73775 15847 21099",
+			"SPELL_AURA_APPLIED 36096 66009 73061 21098 22067"
 		)
 	elseif msg:match(L.RoundComplete) then -- victory
 		timerCombatStart:Start()
 		-- DBM:EndCombat(self)
-		-- self:Stop()
+		self:Stop()
 		DBM:AddSpecialEventToTranscriptorLog("Completed round" .. self.vb.roundCounter or "nil")
-		timerToRessurect:Stop()
-
 		self:Unschedule(resurrectionTicker)
 		self:UnregisterShortTermEvents()
 
@@ -95,9 +116,8 @@ function mod:CHAT_MSG_RAID_BOSS_EMOTE(msg)
 		end
 	elseif msg:find(L.RoundFailed) then -- wipe
 		-- DBM:EndCombat(self, true)
-		-- self:Stop()
+		self:Stop()
 		DBM:AddSpecialEventToTranscriptorLog("Wiped on round" .. self.vb.roundCounter or "nil")
-		timerToRessurect:Stop()
 		self:Unschedule(resurrectionTicker)
 		self:UnregisterShortTermEvents()
 
