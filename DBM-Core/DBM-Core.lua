455806ede06551605db45528c8bdeb28586e7f19
Zidras <10605951+Zidras@users.noreply.github.com>
2024-12-31 16:41:37 +0000

    Core: add custom difficulty for Warmane events auto-logging
    Index as per https://github.com/Zidras/DBM-Warmane/commit/9e1deb0250cd6a2485c3590e77c1e573a570c4f3

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -82,7 +82,7 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20241228174232"),
+	Revision = parseCurseDate("20241231163959"),
 	DisplayVersion = "10.1.13 alpha", -- the string that is shown as version
 	ReleaseRevision = releaseDate(2024, 07, 20) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
@@ -5464,7 +5464,9 @@ do
 			return true
 		end
 		--Current player level non Mythic raid
-		if self.Options.LogCurrentRaids and instanceDifficultyBylevel[LastInstanceMapID] and (instanceDifficultyBylevel[LastInstanceMapID][1] >= playerLevel) and (instanceDifficultyBylevel[LastInstanceMapID][2] == 3) and difficultyIndex ~= 16 then
+		if self.Options.LogCurrentRaids and instanceDifficultyBylevel[LastInstanceMapID] and (instanceDifficultyBylevel[LastInstanceMapID][1] >= playerLevel) and (instanceDifficultyBylevel[LastInstanceMapID][2] == 3) and difficultyIndex ~= 16
+		or difficultyIndex == 18 -- Custom (Warmane Events)
+		then
 			return true
 		end
 		--Trivial raid (ie one below players level)
