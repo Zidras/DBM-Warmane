d346c864f4bf78728fa4e8ac4a9e9a39f028ac06
Zidras <10605951+Zidras@users.noreply.github.com>
2025-01-03 20:26:52 +0000

    Warmane-TowerDefense: add GUI titles

--- DBM-WorldEvents/WarmaneEvents/TowerDefense.lua
+++ DBM-WorldEvents/WarmaneEvents/TowerDefense.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("WarmaneTowerDefense", "DBM-WorldEvents", 2)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20250103191635")
+mod:SetRevision("20250103202530")
 mod:SetUsedIcons(1, 2, 3, 4, 5)
 mod:SetHotfixNoticeRev(20241231000000)
 mod.noStatistics = true -- needed to avoid Start/End chat messages, as well as other interactions not really suited for this event (wave based)
@@ -33,26 +33,26 @@ mod:RemoveOption("HealthFrame")
 mod:AddBoolOption("TimerRound", true, "timer", nil, 1, 1)
 
 -- Trash
+mod:AddTimerLine(DBM_COMMON_L.ADDS)
 local specWarnSpellReflectDispel	= mod:NewSpecialWarningDispel(36096, "MagicDispeller", nil, nil, 1, 2)
 local specWarnHandOfProtectionDispel= mod:NewSpecialWarningDispel(66009, "ImmunityDispeller", nil, nil, 1, 2)
 
 -- Bosses
--- Shade of Aran (400024)
-local specWarnCounterspellStopCast	= mod:NewSpecialWarningCast(29961, "SpellCaster", nil, nil, 1, 2)
-local specWarnIceBurstRun			= mod:NewSpecialWarningRun(69108, "Melee", nil, nil, 4, 2)
-local specWarnLivingBombMoveAway	= mod:NewSpecialWarningMoveAway(20475, "Melee", nil, nil, 1, 2)
+mod:AddTimerLine(BOSSES)
+mod:AddTimerLine("Dragons")
+-- Dragons
+local timerTailSweep				= mod:NewVarTimer("v15-20", 15847, nil, nil, nil, 2) -- 5s variance [15-20] (Lordaeron Horde [2024-12-27]@[13:17:37]) - "Tail Sweep-15847-npc:6836-432 = pull:1152.8, 18.2, 16.3, 18.7, 16.7, 16.3, 16.3, 17.3, 19.6, 16.4"
 
-mod:AddRangeFrameOption(15, 69108)
-
+mod:AddTimerLine("Azuregos")
 -- Azuregos (400052)
 local warnReflection				= mod:NewSpellAnnounce(22067, 2)
 
 local specWarnChillDispel			= mod:NewSpecialWarningDispel(21098, "MagicDispeller", nil, nil, 1, 2)
 
-local timerTailSweep				= mod:NewVarTimer("v15-20", 15847, nil, nil, nil, 2) -- 5s variance [15-20] (Lordaeron Horde [2024-12-27]@[13:17:37]) - "Tail Sweep-15847-npc:6836-432 = pull:1152.8, 18.2, 16.3, 18.7, 16.7, 16.3, 16.3, 17.3, 19.6, 16.4"
 local timerFrostBreath				= mod:NewVarTimer("v15-20", 21099, nil, "Tank|Healer", nil, 5, nil, DBM_COMMON_L.TANK_ICON) -- 5s variance [15-20] (Lordaeron Horde [2024-12-27]@[13:17:37]) - "Frost Breath-21099-npc:6836-432 = pull:1148.1, 18.7, 16.1, 18.6, 15.9, 19.5, 16.5, 15.2, 20.0, 15.8"
 local timerNextChill				= mod:NewNextTimer(15, 21098, nil, nil, nil, 2) -- (Lordaeron Horde [2024-12-27]@[13:17:37]) - "Chill-21098-npc:6836-432 = pull:1160.1[+41], 14.7[+10], 0.1[+20], 15.0[+38], 14.9[+26], 15.0[+18], 15.0[+21], 14.9[+21], 15.0[+18], 14.9[+8], 0.1[+18], 14.9, 0.1[+4], 14.9"
 
+mod:AddTimerLine("Ysondre")
 -- Ysondre (400025)
 local warnMindControlSoon			= mod:NewSoonAnnounce(28410, 4)
 local warnMindControl				= mod:NewTargetNoFilterAnnounce(28410, 3)
@@ -63,11 +63,22 @@ local timerNextMindControl			= mod:NewNextTimer(45, 28410, nil, nil, nil, 3) -- 
 mod:AddSetIconOption("SetIconOnMindControl", 28410, true, 0, {1, 2, 3, 4, 5})
 mod:AddBoolOption("EqUneqWeapons", mod:IsDps(), nil, nil, nil, nil, 28410)
 
+mod:AddTimerLine("Shade of Aran")
+-- Shade of Aran (400024)
+local specWarnCounterspellStopCast	= mod:NewSpecialWarningCast(29961, "SpellCaster", nil, nil, 1, 2)
+local specWarnIceBurstRun			= mod:NewSpecialWarningRun(69108, "Melee", nil, nil, 4, 2)
+local specWarnLivingBombMoveAway	= mod:NewSpecialWarningMoveAway(20475, "Melee", nil, nil, 1, 2)
+
+mod:AddRangeFrameOption(15, 69108)
+
+mod:AddTimerLine("Ragnaros")
 -- Ragnaros (400049)
 local specWarnWrathOfRagnarosRun	= mod:NewSpecialWarningRun(20566, "Melee", nil, nil, 4, 2)
 
+mod:AddTimerLine("Illidan Stormrage")
 -- Illidan Stormrage (400022)
 
+mod:AddTimerLine("Void Reaver")
 -- Void Reaver (400051)
 local warnWarStompSoon				= mod:NewSoonAnnounce(41534, 2)
 
