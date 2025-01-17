16f8e50ae05864232c44d59150c1aaf17710b6b5
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2025-01-17 21:11:44 +0200

    Karazhan: Update timers (#238)
    * Karazhan: Update timers
    
    Maiden of Virtue: Add keep arg to Repentance CD
    Netherspite: Fix icon texture
    
    * Maiden of Virtue: Update Repentance timer

--- DBM-Karazhan/Netherspite.lua
+++ DBM-Karazhan/Netherspite.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Netherspite", "DBM-Karazhan")
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20220518110528")
+mod:SetRevision("20250116122738")
 mod:SetCreatureID(15689)
 
 mod:SetModelID(15363)
@@ -13,15 +13,15 @@ mod:RegisterEventsInCombat(
 	"CHAT_MSG_RAID_BOSS_EMOTE"
 )
 
-local warningPortal			= mod:NewAnnounce("warningPortal", 1, "135743")
-local warningBanish			= mod:NewAnnounce("warningBanish", 1, "136135")
+local warningPortal			= mod:NewAnnounce("warningPortal", 1, "Interface\\Icons\\Spell_Arcane_PortalIronforge")
+local warningBanish			= mod:NewAnnounce("warningBanish", 1, "Interface\\Icons\\Spell_Shadow_Cripple")
 local warningBreathCast		= mod:NewCastAnnounce(38523, 2)
 local warningVoid			= mod:NewSpellAnnounce(37063, 4)
 
 local specWarnVoid			= mod:NewSpecialWarningGTFO(30533, nil, nil, nil, 1, 6)
 
-local timerPortalPhase		= mod:NewTimer(61.5, "timerPortalPhase", "135743", nil, nil, 6)
-local timerBanishPhase		= mod:NewTimer(30, "timerBanishPhase", "136135", nil, nil, 6)
+local timerPortalPhase		= mod:NewTimer(61.5, "timerPortalPhase", "Interface\\Icons\\Spell_Arcane_PortalIronforge", nil, nil, 6)
+local timerBanishPhase		= mod:NewTimer(30, "timerBanishPhase", "Interface\\Icons\\Spell_Shadow_Cripple", nil, nil, 6)
 local timerBreathCast		= mod:NewCastTimer(2.5, 38523, nil, nil, nil, 3)
 
 local berserkTimer			= mod:NewBerserkTimer(540)
