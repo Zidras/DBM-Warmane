09d6f6a3ef552244bd250b28216d5d6612792fb9
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2025-02-13 11:21:04 +0000

    Fathomlord - Add timers

--- DBM-Serpentshrine/Fathomlord.lua
+++ DBM-Serpentshrine/Fathomlord.lua
@@ -10,21 +10,35 @@ mod:RegisterCombat("combat_yell", L.YellPull)
 
 mod:RegisterEventsInCombat(
 	"SPELL_AURA_APPLIED 38451 38452 38455",
-	"SPELL_CAST_START 38330",
-	"SPELL_SUMMON 38236"
+	"SPELL_CAST_START 38330 38445",
+	"SPELL_CAST_SUCCESS 38441",
+	"SPELL_SUMMON 38306 38304 38236"
 )
 
-local warnCariPower		= mod:NewSpellAnnounce(38451, 3)
-local warnTidalPower	= mod:NewSpellAnnounce(38452, 3)
-local warnSharPower		= mod:NewSpellAnnounce(38455, 3)
+local warnCariPower				= mod:NewSpellAnnounce(38451, 3)
+local warnTidalPower			= mod:NewSpellAnnounce(38452, 3)
+local warnSharPower				= mod:NewSpellAnnounce(38455, 3)
 
-local specWarnHeal		= mod:NewSpecialWarningInterrupt(38330, "HasInterrupt", nil, nil, 1, 2)
-local specWarnTotem		= mod:NewSpecialWarningSwitch(38236, "Dps", nil, nil, 1, 2)
+local specWarnHeal				= mod:NewSpecialWarningInterrupt(38330, "HasInterrupt", nil, nil, 1, 2)
+local specWarnCleansingTotem	= mod:NewSpecialWarningSwitch(38306, "Dps", nil, nil, 1, 2)
+local specWarnEarthbindTotem	= mod:NewSpecialWarningSwitch(38304, "Dps", nil, nil, 1, 2)
+local specWarnSpitfireTotem		= mod:NewSpecialWarningSwitch(38236, "Dps", nil, nil, 1, 2)
 
-local berserkTimer		= mod:NewBerserkTimer(600)
+local timerCataclysmicBolt		= mod:NewNextTimer(10, 38441, nil, "Healer", nil, 2)
+local timerSearNova				= mod:NewVarTimer("v20-40", 38445, nil, nil, nil, 2)
+local timerCleansingTotem		= mod:NewNextTimer(30, 38306, nil, nil, nil, 5)
+local timerEarthbindTotem		= mod:NewNextTimer(45, 38304, nil, nil, nil, 5)
+local timerSpitfireTotem		= mod:NewNextTimer(60, 38236, nil, nil, nil, 5)
+
+local berserkTimer				= mod:NewBerserkTimer(600)
 
 function mod:OnCombatStart(delay)
 	berserkTimer:Start(-delay)
+	timerCataclysmicBolt:Start(-delay)
+	timerSearNova:Start(-delay)
+	timerCleansingTotem:Start(-delay)
+	timerEarthbindTotem:Start(-delay)
+	timerSpitfireTotem:Start(-delay)
 end
 
 function mod:SPELL_AURA_APPLIED(args)
@@ -32,6 +46,9 @@ function mod:SPELL_AURA_APPLIED(args)
 		warnCariPower:Show()
 	elseif args.spellId == 38452 then
 		warnTidalPower:Show()
+		timerCleansingTotem:Cancel()
+		timerEarthbindTotem:Cancel()
+		timerSpitfireTotem:Start(27) -- Spitfire Totem has a new timer
 	elseif args.spellId == 38455 then
 		warnSharPower:Show()
 	end
@@ -43,12 +60,34 @@ function mod:SPELL_CAST_START(args)
 			specWarnHeal:Show(args.sourceName)
 			specWarnHeal:Play("kickcast")
 		end
+	elseif args.spellId == 38445 then
+		timerSearNova:Start()
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 38441 then
+		timerCataclysmicBolt:Start()
 	end
 end
 
 function mod:SPELL_SUMMON(args)
-	if args.spellId == 38236 then
-		specWarnTotem:Show()
-		specWarnTotem:Play("attacktotem")
+	if args.spellId == 38306 then
+		specWarnCleansingTotem:Show()
+		specWarnCleansingTotem:Play("attacktotem")
+		timerCleansingTotem:Start()
+	elseif args.spellId == 38304 then
+		specWarnEarthbindTotem:Show()
+		specWarnEarthbindTotem:Play("attacktotem")
+		timerEarthbindTotem:Start()
+	elseif args.spellId == 38236 then
+		specWarnSpitfireTotem:Show()
+		specWarnSpitfireTotem:Play("attacktotem")
+		local cId = self:GetCIDFromGUID(args.sourceGUID)
+		if cId == 21965 then -- Tidalvess
+			timerSpitfireTotem:Start()
+		elseif cId == 21214 then -- Karathress
+			timerSpitfireTotem:Start(27)
+		end
 	end
 end
