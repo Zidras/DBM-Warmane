7ee03ccb4abd7ee6b10d59c538a89a6b13469d60
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2025-02-24 15:14:59 +0000

    Kargath: Update boss mod

--- DBM-Party-BC/Hellfire_ShatteredHalls/Kargath.lua
+++ DBM-Party-BC/Hellfire_ShatteredHalls/Kargath.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod.statTypes = "normal,heroic,mythic"
 
-mod:SetRevision("20220518110528")
+mod:SetRevision("20250224151524")
 mod:SetCreatureID(16808)
 
 mod:SetModelID(19799)
@@ -11,20 +11,22 @@ mod:SetModelOffset(-0.4, 0.1, -0.4)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"UNIT_SPELLCAST_START"
+	"SPELL_CAST_SUCCESS 30739 35429"
 )
 
 --134170 Some Random Orc Icon. Could not find red fel orc icon. Only green orcs or brown orcs. Brown closer to red than green is.
-local warnHeathenGuard			= mod:NewAnnounce("warnHeathen", 2, 134170)
-local warnReaverGuard			= mod:NewAnnounce("warnReaver", 2, 134170)
-local warnSharpShooterGuard		= mod:NewAnnounce("warnSharpShooter", 2, 134170)
+local warnHeathenGuard			= mod:NewAnnounce("warnHeathen", 2, "Interface\\Icons\\Inv_Misc_Head_Orc_01")
+local warnReaverGuard			= mod:NewAnnounce("warnReaver", 2, "Interface\\Icons\\Inv_Misc_Head_Orc_01")
+local warnSharpShooterGuard		= mod:NewAnnounce("warnSharpShooter", 2, "Interface\\Icons\\Inv_Misc_Head_Orc_01")
 
 local specWarnBladeDance		= mod:NewSpecialWarningSpell(30739, nil, nil, nil, 2, 2)
+local specWarnSweepingStrikes	= mod:NewSpecialWarningSpell(35429, nil, nil, nil, 1, 2)
 
-local timerHeathenCD			= mod:NewTimer(21, "timerHeathen", 134170, nil, nil, 1)
-local timerReaverCD				= mod:NewTimer(21, "timerReaver", 134170, nil, nil, 1)
-local timerSharpShooterCD		= mod:NewTimer(21, "timerSharpShooter", 134170, nil, nil, 1)
-local timerBladeDanceCD			= mod:NewCDTimer(35, 30739, nil, nil, nil, 2)
+local timerHeathenCD			= mod:NewTimer(21, "timerHeathen", "Interface\\Icons\\Inv_Misc_Head_Orc_01", nil, nil, 1)
+local timerReaverCD				= mod:NewTimer(21, "timerReaver", "Interface\\Icons\\Inv_Misc_Head_Orc_01", nil, nil, 1)
+local timerSharpShooterCD		= mod:NewTimer(21, "timerSharpShooter", "Interface\\Icons\\Inv_Misc_Head_Orc_01", nil, nil, 1)
+local timerBladeDanceCD			= mod:NewNextTimer(29, 30739, nil, nil, nil, 2, nil, nil, true)
+local timerSweepingStrikesCD	= mod:NewNextTimer(22, 35429, nil, nil, nil, 5, nil, nil, true)
 
 mod.vb.addSet = 0
 mod.vb.addType = 0
@@ -32,13 +34,13 @@ mod.vb.addType = 0
 local function Adds(self)
 	self.vb.addSet = self.vb.addSet + 1
 	self.vb.addType = self.vb.addType + 1
-	if self.vb.addType == 1 then--Heathen
+	if self.vb.addType == 1 then -- Heathen
 		warnHeathenGuard:Show(self.vb.addSet.."-"..self.vb.addType)
 		timerReaverCD:Start()
-	elseif self.vb.addType == 2 then--Reaver
+	elseif self.vb.addType == 2 then -- Reaver
 		warnReaverGuard:Show(self.vb.addSet.."-"..self.vb.addType)
 		timerSharpShooterCD:Start()
-	elseif self.vb.addType == 3 then--SharpShooter
+	elseif self.vb.addType == 3 then -- SharpShooter
 		warnSharpShooterGuard:Show(self.vb.addSet.."-"..self.vb.addType)
 		timerHeathenCD:Start()
 		self.vb.addType = 0
@@ -50,21 +52,18 @@ function mod:OnCombatStart(delay)
 	self.vb.addSet = 0
 	self.vb.addType = 0
 	timerHeathenCD:Start(27.5-delay)
-	self:Schedule(27.5, Adds, self)--When reaches stairs, not when enters/spawns way down hallway.
-	timerBladeDanceCD:Start(72-delay)
+	self:Schedule(27.5, Adds, self) -- When reaches stairs, not when enters/spawns way down hallway.
+	timerBladeDanceCD:Start(-delay)
+	timerSweepingStrikesCD:Start(11-delay)
 end
 
---Change to no sync if blizz adds IEEU(boss1)
-function mod:UNIT_SPELLCAST_START(_, spellName)
-   if spellName == GetSpellInfo(30738) then -- Blade Dance Targeting
-		self:SendSync("BladeDance")
-	end
-end
-
-function mod:OnSync(msg)
-	if msg == "BladeDance" and self:AntiSpam(3, 1) then
+function mod:SPELL_CAST_SUCCESS(args)
+	if args.spellId == 30739 and self:AntiSpam(3, 1) then -- Blade Dance
 		specWarnBladeDance:Show()
 		timerBladeDanceCD:Start()
 		specWarnBladeDance:Play("aesoon")
+	elseif args.spellId == 35429 then -- Sweeping Strikes
+		specWarnSweepingStrikes:Show()
+		timerSweepingStrikesCD:Start()
 	end
 end
