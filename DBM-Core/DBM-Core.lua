25f793b3138400440f1c990760bbe97b01cb584c
Wardool <wardoulx@gmail.com>
2025-06-29 16:03:43 +0200

    Update version and release date to June 29, 2025
    Bumped the DBM revision and release date to reflect the latest stable version as of June 29, 2025.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -64,9 +64,9 @@ local function currentFullDate()
 end
 
 DBM = {
-	Revision = parseCurseDate("20250624101610"),
+	Revision = parseCurseDate("20250629160322"),
 	DisplayVersion = "10.1.13 - WoE Edition", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2025, 06, 24) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	ReleaseRevision = releaseDate(2025, 06, 29) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 
 local fakeBWVersion = 7558
