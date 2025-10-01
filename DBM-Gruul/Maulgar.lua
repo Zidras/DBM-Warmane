849a08c18768d1114bc7f1c3ed625b5cce2e6bb3
Zidras <10605951+Zidras@users.noreply.github.com>
2025-10-01 20:54:15 +0100

    Raids-BC-Gruul: remove non-wotlk ids

--- DBM-Gruul/Maulgar.lua
+++ DBM-Gruul/Maulgar.lua
@@ -3,7 +3,7 @@ local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("20250929220131")
 mod:SetCreatureID(18831, 18832, 18834, 18835, 18836)
-mod:SetEncounterID(649, 2455)
+mod:SetEncounterID(649)
 
 mod:SetModelID(18831)
 mod:RegisterCombat("combat")
