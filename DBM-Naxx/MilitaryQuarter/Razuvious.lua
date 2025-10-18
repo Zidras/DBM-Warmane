38ea99192ebc3c9b5c5daf772e09cfde5f542947
Fanquar <41789276+Fanquar@users.noreply.github.com>
2025-10-18 12:15:29 +0200

    Update revision

--- DBM-Naxx/MilitaryQuarter/Razuvious.lua
+++ DBM-Naxx/MilitaryQuarter/Razuvious.lua
@@ -1,7 +1,7 @@
 local mod	= DBM:NewMod("Razuvious", "DBM-Naxx", 4)
 local L		= mod:GetLocalizedStrings()
 
-mod:SetRevision("20251018085532")
+mod:SetRevision("20251018121532")
 mod:SetCreatureID(16061)
 mod:SetEncounterID(1113)
 
